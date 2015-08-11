f30a7b7f09d96b4f40491f0604d42008b8326a3f
Andrea <massironi.andrea@gmail.com>
2015-08-12 00:57:44 +0200

    update to new format, and HT and redHT new variables added

--- Gardener/python/variables/WWVar.py
+++ Gardener/python/variables/WWVar.py
@@ -39,12 +39,16 @@ class WWVarFiller(TreeCloner):
 
         # does that work so easily and give new variable itree and otree?
         self.connect(tree,input)
-        newbranches = ['pTWW']
+        newbranches = ['pTWW', 'HT', 'redHT']
         self.clone(output,newbranches)
 
         pTWW    = numpy.ones(1, dtype=numpy.float32)
+        HT      = numpy.ones(1, dtype=numpy.float32)
+        redHT   = numpy.ones(1, dtype=numpy.float32)
 
         self.otree.Branch('pTWW'  , pTWW  , 'pTWW/F')
+        self.otree.Branch('HT'    , HT    , 'HT/F')
+        self.otree.Branch('redHT' , redHT , 'redHT/F')
 
         nentries = self.itree.GetEntries()
         print 'Total number of entries: ',nentries 
@@ -70,23 +74,41 @@ class WWVarFiller(TreeCloner):
             if i > 0 and i%step == 0.:
                 print i,'events processed.'
 
-            pt1 = itree.pt1
-            pt2 = itree.pt2
-            phi1 = itree.phi1
-            phi2 = itree.phi2
+            pt1 = itree.std_vector_lepton_pt[0]
+            pt2 = itree.std_vector_lepton_pt[1]
+            phi1 = itree.std_vector_lepton_pt[0]
+            phi2 = itree.std_vector_lepton_pt[1]
 
 
             met = itree.pfType1Met
             metphi = itree.pfType1Metphi
-
             #met = itree.pfmet
             #metphi = itree.pfmetphi
-
-            WW = ROOT.WW(pt1, pt2, phi1, phi2, met, metphi)
-
-            pTWW[0]   = WW.pTWW()
- 
+            
+            if pt2 > 0 :
+              WW = ROOT.WW(pt1, pt2, phi1, phi2, met, metphi)
+              pTWW[0]   = WW.pTWW()
+            else :
+              pTWW[0]   = -1
+  
             #print "dphill = ", WW.dphill()
+            
+            # calculate HT with leptons and jets
+            HT[0] = 0.0
+            redHT[0] = 0.0
+            for iLep in range(itree.std_vector_lepton_pt.size()) :
+              if itree.std_vector_lepton_pt[iLep] > 0 :
+                HT[0] += itree.std_vector_lepton_pt[iLep]
+                if iLep >= 2 :   # exclude the first two leptons
+                  redHT[0] += itree.std_vector_lepton_pt[iLep]
+               
+            for iJet in range(itree.std_vector_jet_pt.size()) :
+              if itree.std_vector_jet_pt[iJet] > 0 :
+                HT[0] += itree.std_vector_jet_pt[iJet]
+                redHT[0] += itree.std_vector_jet_pt[iJet]
+            
+            HT[0] += met
+            #redHT[0] --> redHT doesn't use met
             
             otree.Fill()
 
