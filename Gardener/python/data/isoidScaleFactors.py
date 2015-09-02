d043b70ef1ab86c1f90fd28567dd20b7a4dd4495
Andrea <massironi.andrea@gmail.com>
2015-09-02 10:33:44 +0200

    new

---
+++ Gardener/python/data/isoidScaleFactors.py
@@ -0,0 +1,17 @@
+#
+# isolation and id scale factors for leptons
+#
+
+# isoidScaleFactors = {}
+
+isoidScaleFactors['ele'] = {
+                          #   pt           eta
+                          [( 0.0, 10.0), (0.0, 1.5)] : ( 0.989, 0.980, 0.999 ),
+                          [(10.0, 20.0), (0.0, 1.5)] : ( 0.989, 0.980, 0.999 ),
+                         }
+
+isoidScaleFactors['mu'] = {
+                          #   pt           eta
+                          [( 0.0, 10.0), (0.0, 1.5)] : ( 0.989, 0.980, 0.999 ),
+                          [(10.0, 20.0), (0.0, 1.5)] : ( 0.989, 0.980, 0.999 ),
+                         }
