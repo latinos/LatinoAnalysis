26b0c531a380f6039d19f66b48e0a6b842066b69
xjanssen <xavier.janssen@cern.ch>
2016-02-29 16:43:25 +0100

    wwSel

--- Gardener/python/Gardener_cfg.py
+++ Gardener/python/Gardener_cfg.py
@@ -726,5 +726,12 @@ Steps= {
            },
 
 
+  'wwSel'    : {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : True  ,
+                  'command'    : 'gardener.py filter -f \' mll>12 && std_vector_lepton_pt[0]>20 && std_vector_lepton_pt[1]>10 && std_vector_lepton_pt[2]<10 && metPfType1 > 20 && ptll > 30 && (std_vector_lepton_flavour[0] * std_vector_lepton_flavour[1] == -11*13) \' '
+           },
+
 }
 
