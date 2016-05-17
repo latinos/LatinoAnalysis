4e0ba38fe3c4e9ca20b2f07a5c0a71604cfb4365
local user <xjanssen@m0.iihe.ac.be>
2016-05-17 10:51:08 +0200

    Add TT smaple

--- Gardener/python/Gardener_cfg.py
+++ Gardener/python/Gardener_cfg.py
@@ -341,6 +341,7 @@ Steps= {
                                   'TTTo2L2Nu','TTWJetsToLNu',
                                   'ST_t-channel_antitop','ST_t-channel_top',
                                   'ST_tW_antitop','ST_tW_top',
+                                  'TTToSemiLeptonic','TT',
                                   # VV (including WW) 
                                   'WWTo2L2Nu','GluGluWWTo2L2Nu_MCFM','GluGluWWTo2L2NuHiggs_MCFM',
                                   'WZTo3LNu',
