f28f590a6ed4a1b17de50edb97cfc8c76e08cfd6
amassiro <massironi.andrea@gmail.com>
2016-07-23 14:44:13 +0200

    test

---
+++ Gardener/python/data/idiso/PrepareSF.cxx
@@ -0,0 +1,157 @@
+
+{ 
+  
+  -2.500          -2.450         +20.000        +2000.000       0.718   /  0.555          =      1.29369
+  -2.450          -2.400         +20.000        +2000.000       0.869   /  0.800          =      1.08625
+  -2.400          -2.300         +20.000        +2000.000       0.947   /  0.939          =      1.00852
+  -2.300          -2.200         +20.000        +2000.000       0.965   /  0.955          =      1.01047
+  -2.200          -2.000         +20.000        +2000.000       0.969   /  0.964          =      1.00519
+  -2.000          -1.800         +20.000        +2000.000       0.965   /  0.967          =      0.997932
+  -1.800          -1.630         +20.000        +2000.000       0.956   /  0.964          =      0.991701
+  -1.630          -1.566         +20.000        +2000.000       0.949   /  0.962          =      0.986486
+  -1.566          -1.444         +20.000        +2000.000       0.851   /  0.885          =      0.961582
+  -1.444          -1.200         +20.000        +2000.000       0.962   /  0.975          =      0.986667
+  -1.200          -1.000         +20.000        +2000.000       0.956   /  0.978          =      0.977505
+  -1.000          -0.600         +20.000        +2000.000       0.950   /  0.980          =      0.969388
+  -0.600          -0.400         +20.000        +2000.000       0.948   /  0.981          =      0.966361
+  -0.400          -0.200         +20.000        +2000.000       0.945   /  0.981          =      0.963303
+  -0.200          +0.000         +20.000        +2000.000       0.936   /  0.975          =      0.960000
+  +0.000          +0.200         +20.000        +2000.000       0.943   /  0.976          =      0.966189
+  +0.200          +0.400         +20.000        +2000.000       0.962   /  0.982          =      0.979633
+  +0.400          +0.600         +20.000        +2000.000       0.959   /  0.982          =      0.976578
+  +0.600          +1.000         +20.000        +2000.000       0.963   /  0.982          =      0.980652
+  +1.000          +1.200         +20.000        +2000.000       0.967   /  0.980          =      0.986735
+  +1.200          +1.444         +20.000        +2000.000       0.963   /  0.976          =      0.986680
+  +1.444          +1.566         +20.000        +2000.000       0.862   /  0.888          =      0.970721
+  +1.566          +1.630         +20.000        +2000.000       0.958   /  0.968          =      0.989669
+  +1.630          +1.800         +20.000        +2000.000       0.965   /  0.969          =      0.995872
+  +1.800          +2.000         +20.000        +2000.000       0.964   /  0.974          =      0.989733
+  +2.000          +2.200         +20.000        +2000.000       0.968   /  0.973          =      0.994861
+  +2.200          +2.300         +20.000        +2000.000       0.961   /  0.968          =      0.992769
+  +2.300          +2.400         +20.000        +2000.000       0.927   /  0.959          =      0.966632
+  +2.400          +2.450         +20.000        +2000.000       0.782   /  0.863          =      0.906141
+  +2.450          +2.500         +20.000        +2000.000       0.583   /  0.683          =      0.853587
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  abs(std_vector_lepton_flavour[0]) == 11  &&     (  std_vector_lepton_eta[0]>                   &&     std_vector_lepton_eta[0]<       ) *      
+  
+  
+  
+  abs(std_vector_lepton_flavour[0]) == 13 + (   \
+  abs(std_vector_lepton_flavour[0]) == 11   &&    \
+  (  std_vector_lepton_eta[0]>   -2.500     &&     std_vector_lepton_eta[0]<       -2.450        )   *  1.29369       +      \
+  (  std_vector_lepton_eta[0]>   -2.450     &&     std_vector_lepton_eta[0]<       -2.400        )   *  1.08625       +      \
+  (  std_vector_lepton_eta[0]>   -2.400     &&     std_vector_lepton_eta[0]<       -2.300        )   *  1.00852       +      \
+  (  std_vector_lepton_eta[0]>   -2.300     &&     std_vector_lepton_eta[0]<       -2.200        )   *  1.01047       +      \
+  (  std_vector_lepton_eta[0]>   -2.200     &&     std_vector_lepton_eta[0]<       -2.000        )   *  1.00519       +      \
+  (  std_vector_lepton_eta[0]>   -2.000     &&     std_vector_lepton_eta[0]<       -1.800        )   *  0.997932      +      \
+  (  std_vector_lepton_eta[0]>   -1.800     &&     std_vector_lepton_eta[0]<       -1.630        )   *  0.991701      +      \
+  (  std_vector_lepton_eta[0]>   -1.630     &&     std_vector_lepton_eta[0]<       -1.566        )   *  0.986486      +      \
+  (  std_vector_lepton_eta[0]>   -1.566     &&     std_vector_lepton_eta[0]<       -1.444        )   *  0.961582      +      \
+  (  std_vector_lepton_eta[0]>   -1.444     &&     std_vector_lepton_eta[0]<       -1.200        )   *  0.986667      +      \
+  (  std_vector_lepton_eta[0]>   -1.200     &&     std_vector_lepton_eta[0]<       -1.000        )   *  0.977505      +      \
+  (  std_vector_lepton_eta[0]>   -1.000     &&     std_vector_lepton_eta[0]<       -0.600        )   *  0.969388      +      \
+  (  std_vector_lepton_eta[0]>   -0.600     &&     std_vector_lepton_eta[0]<       -0.400        )   *  0.966361      +      \
+  (  std_vector_lepton_eta[0]>   -0.400     &&     std_vector_lepton_eta[0]<       -0.200        )   *  0.963303      +      \
+  (  std_vector_lepton_eta[0]>   -0.200     &&     std_vector_lepton_eta[0]<       +0.000        )   *  0.960000      +      \
+  (  std_vector_lepton_eta[0]>   +0.000     &&     std_vector_lepton_eta[0]<       +0.200        )   *  0.966189      +      \
+  (  std_vector_lepton_eta[0]>   +0.200     &&     std_vector_lepton_eta[0]<       +0.400        )   *  0.979633      +      \
+  (  std_vector_lepton_eta[0]>   +0.400     &&     std_vector_lepton_eta[0]<       +0.600        )   *  0.976578      +      \
+  (  std_vector_lepton_eta[0]>   +0.600     &&     std_vector_lepton_eta[0]<       +1.000        )   *  0.980652      +      \
+  (  std_vector_lepton_eta[0]>   +1.000     &&     std_vector_lepton_eta[0]<       +1.200        )   *  0.986735      +      \
+  (  std_vector_lepton_eta[0]>   +1.200     &&     std_vector_lepton_eta[0]<       +1.444        )   *  0.986680      +      \
+  (  std_vector_lepton_eta[0]>   +1.444     &&     std_vector_lepton_eta[0]<       +1.566        )   *  0.970721      +      \
+  (  std_vector_lepton_eta[0]>   +1.566     &&     std_vector_lepton_eta[0]<       +1.630        )   *  0.989669      +      \
+  (  std_vector_lepton_eta[0]>   +1.630     &&     std_vector_lepton_eta[0]<       +1.800        )   *  0.995872      +      \
+  (  std_vector_lepton_eta[0]>   +1.800     &&     std_vector_lepton_eta[0]<       +2.000        )   *  0.989733      +      \
+  (  std_vector_lepton_eta[0]>   +2.000     &&     std_vector_lepton_eta[0]<       +2.200        )   *  0.994861      +      \
+  (  std_vector_lepton_eta[0]>   +2.200     &&     std_vector_lepton_eta[0]<       +2.300        )   *  0.992769      +      \
+  (  std_vector_lepton_eta[0]>   +2.300     &&     std_vector_lepton_eta[0]<       +2.400        )   *  0.966632      +      \
+  (  std_vector_lepton_eta[0]>   +2.400     &&     std_vector_lepton_eta[0]<       +2.450        )   *  0.906141      +      \
+  (  std_vector_lepton_eta[0]>   +2.450     &&     std_vector_lepton_eta[0]<       +2.500        )   *  0.853587      +      \
+  )   \
+  *   \
+  abs(std_vector_lepton_flavour[1]) == 11   &&    \
+  (  std_vector_lepton_eta[1]>   -2.500     &&     std_vector_lepton_eta[1]<       -2.450        )   *  1.29369       +      \
+  (  std_vector_lepton_eta[1]>   -2.450     &&     std_vector_lepton_eta[1]<       -2.400        )   *  1.08625       +      \
+  (  std_vector_lepton_eta[1]>   -2.400     &&     std_vector_lepton_eta[1]<       -2.300        )   *  1.00852       +      \
+  (  std_vector_lepton_eta[1]>   -2.300     &&     std_vector_lepton_eta[1]<       -2.200        )   *  1.01047       +      \
+  (  std_vector_lepton_eta[1]>   -2.200     &&     std_vector_lepton_eta[1]<       -2.000        )   *  1.00519       +      \
+  (  std_vector_lepton_eta[1]>   -2.000     &&     std_vector_lepton_eta[1]<       -1.800        )   *  0.997932      +      \
+  (  std_vector_lepton_eta[1]>   -1.800     &&     std_vector_lepton_eta[1]<       -1.630        )   *  0.991701      +      \
+  (  std_vector_lepton_eta[1]>   -1.630     &&     std_vector_lepton_eta[1]<       -1.566        )   *  0.986486      +      \
+  (  std_vector_lepton_eta[1]>   -1.566     &&     std_vector_lepton_eta[1]<       -1.444        )   *  0.961582      +      \
+  (  std_vector_lepton_eta[1]>   -1.444     &&     std_vector_lepton_eta[1]<       -1.200        )   *  0.986667      +      \
+  (  std_vector_lepton_eta[1]>   -1.200     &&     std_vector_lepton_eta[1]<       -1.000        )   *  0.977505      +      \
+  (  std_vector_lepton_eta[1]>   -1.000     &&     std_vector_lepton_eta[1]<       -0.600        )   *  0.969388      +      \
+  (  std_vector_lepton_eta[1]>   -0.600     &&     std_vector_lepton_eta[1]<       -0.400        )   *  0.966361      +      \
+  (  std_vector_lepton_eta[1]>   -0.400     &&     std_vector_lepton_eta[1]<       -0.200        )   *  0.963303      +      \
+  (  std_vector_lepton_eta[1]>   -0.200     &&     std_vector_lepton_eta[1]<       +0.000        )   *  0.960000      +      \
+  (  std_vector_lepton_eta[1]>   +0.000     &&     std_vector_lepton_eta[1]<       +0.200        )   *  0.966189      +      \
+  (  std_vector_lepton_eta[1]>   +0.200     &&     std_vector_lepton_eta[1]<       +0.400        )   *  0.979633      +      \
+  (  std_vector_lepton_eta[1]>   +0.400     &&     std_vector_lepton_eta[1]<       +0.600        )   *  0.976578      +      \
+  (  std_vector_lepton_eta[1]>   +0.600     &&     std_vector_lepton_eta[1]<       +1.000        )   *  0.980652      +      \
+  (  std_vector_lepton_eta[1]>   +1.000     &&     std_vector_lepton_eta[1]<       +1.200        )   *  0.986735      +      \
+  (  std_vector_lepton_eta[1]>   +1.200     &&     std_vector_lepton_eta[1]<       +1.444        )   *  0.986680      +      \
+  (  std_vector_lepton_eta[1]>   +1.444     &&     std_vector_lepton_eta[1]<       +1.566        )   *  0.970721      +      \
+  (  std_vector_lepton_eta[1]>   +1.566     &&     std_vector_lepton_eta[1]<       +1.630        )   *  0.989669      +      \
+  (  std_vector_lepton_eta[1]>   +1.630     &&     std_vector_lepton_eta[1]<       +1.800        )   *  0.995872      +      \
+  (  std_vector_lepton_eta[1]>   +1.800     &&     std_vector_lepton_eta[1]<       +2.000        )   *  0.989733      +      \
+  (  std_vector_lepton_eta[1]>   +2.000     &&     std_vector_lepton_eta[1]<       +2.200        )   *  0.994861      +      \
+  (  std_vector_lepton_eta[1]>   +2.200     &&     std_vector_lepton_eta[1]<       +2.300        )   *  0.992769      +      \
+  (  std_vector_lepton_eta[1]>   +2.300     &&     std_vector_lepton_eta[1]<       +2.400        )   *  0.966632      +      \
+  (  std_vector_lepton_eta[1]>   +2.400     &&     std_vector_lepton_eta[1]<       +2.450        )   *  0.906141      +      \
+  (  std_vector_lepton_eta[1]>   +2.450     &&     std_vector_lepton_eta[1]<       +2.500        )   *  0.853587      +      \
+  )   \
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Gardener/python/data/idiso/README.md
@@ -0,0 +1,5 @@
+Code
+====
+
+Prepare scale factors for on-the-fly checks
+
