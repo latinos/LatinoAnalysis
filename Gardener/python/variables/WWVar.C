5d3b8e7825d875dbfa54061cd0d4a992f55391fc
Janssen Xavier <xavier.janssen@uantwerpen.be>
2016-10-26 14:32:45 +0200

    Adding new DYmva for ggH

--- Gardener/python/variables/WWVar.C
+++ Gardener/python/variables/WWVar.C
@@ -16,6 +16,7 @@ public:
  WW(float pt1, float pt2, float eta1, float eta2, float phi1, float phi2, float pidl1, float pidl2, float met, float metphi);
  WW(float pt1, float pt2, float eta1, float eta2, float phi1, float phi2, float met, float metphi);
  WW(float pt1, float pt2, float phi1, float phi2, float met, float metphi);
+ WW(float pt1, float pt2, float eta1, float eta2, float phi1, float phi2, float met, float metphi, float metsum, float jetpt1, float jetpt2, float jeteta1, float jeteta2, float jetphi1, float jetphi2, float jetmass1, float jetmass2);
  virtual ~WW() {}
  
  //! check
@@ -28,11 +29,19 @@ public:
 
  void setLeptons(std::vector<float> invectorpt, std::vector<float> invectoreta, std::vector<float> invectorphi, std::vector<float> invectorflavour);
  
- void setMET  (float met, float metphi);
- void setTkMET(float met, float metphi);
+ void setMET   (float met, float metphi);
+ void setUMET  (float met, float metphi);
+ void setTkMET (float met, float metphi);
  
  //! functions
- float pTWW();
+ float recoil();
+ float jetpt1_cut();
+ float dphilljet_cut();
+ float dphijet1met_cut();
+ float PfMetDivSumMet();
+ float upara();
+ float uperp();
+
  float dphill();
  float mll();
  float pt1();
@@ -41,7 +50,6 @@ public:
  float eta2();
  float phi1();
  float phi2();
- float mT2();  //void functionMT2(int& npar, double* d, double& r, double par[], int flag);
  float yll();
  float ptll();
  float drll();
@@ -62,9 +70,6 @@ public:
  float mth();
  float mcoll();
  float mcollWW();
- float mTi();
- float mTe();
- float choiMass();
  float mR();
  float dphillmet();
  float channel();
@@ -97,8 +102,10 @@ private:
  //! variables
  TLorentzVector L1,L2,L3;
  TLorentzVector MET;
+ TLorentzVector UMET;
  TLorentzVector J1, J2;
  float pid1, pid2;
+ float SumEt ;
  TLorentzVector TkMET;
  
  bool _isOk;
@@ -140,6 +147,35 @@ WW::WW(float pt1, float pt2, float phi1, float phi2, float met, float metphi) {
  _jetOk = 0;
  _isTkMET = false; 
 }
+
+WW::WW(float pt1, float pt2, float eta1, float eta2, float phi1, float phi2, float met, float metphi, float metsum, float jetpt1, float jetpt2, float jeteta1, float jeteta2, float jetphi1, float jetphi2, float jetmass1, float jetmass2) {
+
+ if (pt1>0 && pt2>0) {
+  L1.SetPtEtaPhiM(pt1, eta1, phi1, 0.);
+  L2.SetPtEtaPhiM(pt2, eta2, phi2, 0.);
+  MET.SetPxPyPzE(met, 0., metphi, 0.);
+  float metx = met*cos(metphi);
+  float mety = met*sin(metphi);
+  UMET.SetPxPyPzE(metx, mety, 0., met);
+  SumEt = metsum;
+  _isOk =  true;
+ }
+ else {
+  _isOk = false;
+ }
+ if( jetpt1>0) {
+  J1.SetPtEtaPhiM(jetpt1, jeteta1, jetphi1, jetmass1); //---- NB: jets are treated as massive
+  if( jetpt2>0) {
+   J2.SetPtEtaPhiM(jetpt2, jeteta2, jetphi2, jetmass2); //---- NB: jets are treated as massive
+   _jetOk = 2;
+  }
+  else {
+   _jetOk = 1;
+  }
+ }
+ _isTkMET = false;
+}
+
 
 WW::WW(float pt1, float pt2, float eta1, float eta2, float phi1, float phi2, float met, float metphi) {
  
@@ -257,6 +293,13 @@ void WW::setMET(float met, float metphi) {
  MET.SetPtEtaPhiM(met, 0, metphi, 0.);
 }
 
+void WW::setUMET(float met, float metphi) {
+ float metx = met* cos(metphi);
+ float mety = met* sin(metphi);
+ UMET.SetPxPyPzE(metx, mety, 0., met);
+}
+
+
 void WW::setTkMET(float met, float metphi) {
  TkMET.SetPtEtaPhiM(met, 0, metphi, 0.);
  _isTkMET = true;
@@ -353,9 +396,9 @@ float WW::yll(){
 }
 
 
-float WW::pTWW(){ 
+float WW::recoil(){ 
  if (_isOk) {
-  return (L1+L2+MET).Pt();
+  return fabs((L1+L2+MET).Pt());
  }
  else {
   return -9999.0;
@@ -383,8 +426,8 @@ float WW::drll(){
  } 
 }
 
-float WW::dphilljet(){ 
- if (_isOk && _jetOk >= 1) {
+float WW::dphilljet(){
+ if (_isOk && _jetOk >= 1) { 
   return  fabs( (L1+L2).DeltaPhi(J1) );
  }
  else {
@@ -392,6 +435,34 @@ float WW::dphilljet(){
  }
 }
 
+
+
+float WW::jetpt1_cut(){
+ if (_isOk && _jetOk >= 1 && J1.Pt()>15.0) {
+  return J1.Pt();
+ }
+ else {
+  return -9999.0;
+ }
+}
+
+float WW::dphilljet_cut(){ 
+ if (_isOk && _jetOk >= 1 && J1.Pt()>15.0 ) {
+  return fabs( (L1+L2).DeltaPhi(J1) );
+ }
+ else {
+  return -9999.0;
+ }
+}
+
+float WW::dphijet1met_cut(){
+ if (_isOk && _jetOk >= 1 && J1.Pt()>15.0 ) {
+  return fabs(J1.DeltaPhi(MET));
+ }
+ else {
+  return -9999.0;
+ }
+}
 
 float WW::dphilljetjet(){ 
  if (_isOk && _jetOk >= 2) {
@@ -480,6 +551,44 @@ float WW::pfmet(){
 }
 
 
+float WW::PfMetDivSumMet(){
+
+ if (_isOk) {
+  return MET.Pt()/sqrt(SumEt);
+ }
+ else {
+  return -9999.0;
+ }
+}
+
+
+float WW::upara(){
+
+ if (_isOk) {
+  TLorentzVector utv = (-UMET-(L1+L2));
+  float ut = utv.P();
+  float uphi = utv.DeltaPhi(L1+L2);
+  return ut*cos(uphi);
+ }
+ else {
+  return -9999.0;
+ }
+}
+
+float WW::uperp(){
+
+ if (_isOk) {
+  TLorentzVector utv = (-UMET-(L1+L2));
+  float ut = utv.P();
+  float uphi = utv.DeltaPhi(L1+L2);
+  return ut*sin(uphi);
+ }
+ else {
+  return -9999.0;
+ }
+}
+
+
 float WW::projpfmet(){
  
   if (_isOk) {
@@ -517,10 +626,6 @@ float WW::mpmet(){
   return -9999.0;
  }
 }
-
-
-
-
 
 //---- pt
 float WW::pt1(){
@@ -675,56 +780,12 @@ float WW::mcollWW(){
 }
 
 
-float WW::mTi(){
-
- if (_isOk) {
-  return (L1+L2+MET).M();
- }
- else{
-  return -9999.0;
- }
-
-}
-
-float WW::choiMass(){
-
- //TVector3 METvec, ptllvec, pt1vec, pt2vec;
- //METvec.SetXYZ(MET.X(),MET.Y(),MET.Z());
- //ptllvec.SetXYZ((L1+L2).X(),(L1+L2).Y(),(L1+L2).Z());
- //pt1vec.SetXYZ((L1).X(),(L1).Y(),0);
- //pt2vec.SetXYZ((L2).X(),(L2).Y(),0);
- if (_isOk) {
-  return sqrt( 2*(pt1()*pt1()) + 2*(pt2()*pt2()) + 3*(pt1()*pt2() + (MET.Pt())*(pt1()+pt2()) - MET.Pt()*ptll()*cos(dphillmet()) - 2*pt1()*pt2()*cos(dphill())) );
- }
- else{
-  return -9999.0;
- }
-
-}
-
-
 float WW::mR(){
 
  if (_isOk) {
   return sqrt( 0.5*( mll()*mll() - MET.Pt()*ptll()*cos(dphillmet()) + sqrt( ( mll()*mll() + ptll()*ptll() )*( mll()*mll() + MET.Pt()*MET.Pt()  )  )  )  );
  }
  else{
-  return -9999.0;
- }
-
-}
-
-float WW::mTe(){
-
- if (_isOk) {
-  TLorentzVector L1_enhanced,L2_enhanced;
-
-  L1_enhanced.SetPtEtaPhiM(pt1(), eta1(), phi1(), 80.385);
-  L2_enhanced.SetPtEtaPhiM(pt2(), eta2(), phi2(), 80.385);
-
-  return (L1_enhanced + L2_enhanced+MET).M();
- }
- else {
   return -9999.0;
  }
 
@@ -977,72 +1038,6 @@ void functionMT2(int& npar, double* d, double& r, double par[], int flag){
  
  r = sqrt(maxmt2);
 }
-
-
-float WW::mT2(){
- 
- if (_isOk) {
-  
-  if (VectX->size() != 6) { 
-   VectX->push_back( L1.X()  );
-   VectX->push_back( L1.Y()  );
-   VectX->push_back( L2.X()  );
-   VectX->push_back( L1.Y()  );
-   VectX->push_back( MET.X() );
-   VectX->push_back( MET.Y() );
-  }
-  else {
-   VectX->at(0) = L1.X()  ;
-   VectX->at(1) = L1.Y()  ;
-   VectX->at(2) = L2.X()  ;
-   VectX->at(3) = L1.Y()  ;
-   VectX->at(4) = MET.X() ;
-   VectX->at(5) = MET.Y() ;
-  }
-  
-  double mT2 = 0.;
-  
-  double met = 10;
-  double PI = 3.14159266;
-  
-  const int nParametri = 2;
-  TMinuit minuit(nParametri);
-  minuit.SetFCN(functionMT2);
-  
-  minuit.SetPrintLevel(-1); // quiet
-  
-  met = MET.E();
-  
-  double par[nParametri]={met/2.,0.0};
-  double stepSize[nParametri]={met/100.,0.001};
-  double minVal[nParametri]={met/100.,0.0};
-  double maxVal[nParametri]={30.*met,2.*PI};
-  string parName[nParametri]={"met1","metphi1"};
-  for (int i=0; i<nParametri; i++){
-   minuit.DefineParameter(i,parName[i].c_str(),par[i],stepSize[i],minVal[i],maxVal[i]);
-  }
-  
-  minuit.Migrad();
-  //  double outParametri[nParametri];
-  //  double errParametri[nParametri];
-  //  for (int i=0; i<nParametri; i++){
-  //   minuit.GetParameter(i,outParametri[i],errParametri[i]);
-  //   std::cout << "outParametri[" << i << "] = " << outParametri[i] << " +/- " << errParametri[i] << std::endl;
-  //  }
-  
-  mT2 = minuit.fAmin;
-  //  std::cout << " "  << pxl1 << " ; " << pyl1 << " ; " << pxl2 << " ; " << pyl2 << " ; " << metx << " ; " << mety << " --> mT2 = " << mT2 << std::endl;
-  return mT2;
-  
- }
- else {
-  return -9999.0;
- }
- 
-}
-
-
-
 
 
 //---- to reject Wg*
--- Gardener/python/variables/WWVar.py
+++ Gardener/python/variables/WWVar.py
@@ -39,24 +39,26 @@ class WWVarFiller(TreeCloner):
 
         # does that work so easily and give new variable itree and otree?
         self.connect(tree,input)
-        newbranches = ['pTWW', 'HT', 'redHT', 'mT2', 'mTi', 'mTe', 'choiMass']
+        newbranches = ['dphillmet', 'recoil']
         self.clone(output,newbranches)
 
-        pTWW    = numpy.ones(1, dtype=numpy.float32)
-        HT      = numpy.ones(1, dtype=numpy.float32)
-        redHT   = numpy.ones(1, dtype=numpy.float32)
-        mT2     = numpy.ones(1, dtype=numpy.float32)
-        mTi     = numpy.ones(1, dtype=numpy.float32)
-        mTe     = numpy.ones(1, dtype=numpy.float32)
-        choiMass = numpy.ones(1, dtype=numpy.float32)
+        dphillmet       = numpy.ones(1, dtype=numpy.float32)
+        jetpt1_cut      = numpy.ones(1, dtype=numpy.float32)
+        dphilljet_cut   = numpy.ones(1, dtype=numpy.float32)
+        dphijet1met_cut = numpy.ones(1, dtype=numpy.float32)
+        recoil          = numpy.ones(1, dtype=numpy.float32)
+        PfMetDivSumMet  = numpy.ones(1, dtype=numpy.float32)
+        upara           = numpy.ones(1, dtype=numpy.float32)
+        uperp           = numpy.ones(1, dtype=numpy.float32)
 
-        self.otree.Branch('pTWW'  , pTWW  , 'pTWW/F')
-        self.otree.Branch('HT'    , HT    , 'HT/F')
-        self.otree.Branch('redHT' , redHT , 'redHT/F')
-        self.otree.Branch('mT2'   , mT2   , 'mT2/F')
-        self.otree.Branch('mTi'   , mTi   , 'mTi/F')
-        self.otree.Branch('mTe'   , mTe   , 'mTe/F')
-        self.otree.Branch('choiMass'   , choiMass   , 'choiMass/F')
+        self.otree.Branch('dphillmet'         , dphillmet      , 'dphillmet/F')
+        self.otree.Branch('jetpt1_cut'        , jetpt1_cut     , 'jetpt1_cut/F')
+        self.otree.Branch('dphilljet_cut'     , dphilljet_cut  , 'dphilljet_cut/F')
+        self.otree.Branch('dphijet1met_cut'   , dphijet1met_cut, 'dphijet1met_cut/F')
+        self.otree.Branch('recoil'            , recoil         , 'recoil/F')
+        self.otree.Branch('PfMetDivSumMet'    , PfMetDivSumMet , 'PfMetDivSumMet/F')
+        self.otree.Branch('upara         '    , upara          , 'upara/F')
+        self.otree.Branch('uperp         '    , uperp          , 'uperp/F')
 
         nentries = self.itree.GetEntries()
         print 'Total number of entries: ',nentries 
@@ -82,52 +84,56 @@ class WWVarFiller(TreeCloner):
             if i > 0 and i%step == 0.:
                 print i,'events processed.'
 
-            pt1 = itree.std_vector_lepton_pt[0]
-            pt2 = itree.std_vector_lepton_pt[1]
+            pt1  = itree.std_vector_lepton_pt[0]
+            pt2  = itree.std_vector_lepton_pt[1]
             phi1 = itree.std_vector_lepton_pt[0]
             phi2 = itree.std_vector_lepton_pt[1]
-            eta1 = itree.std_vector_lepton_pt[0]
-            eta2 = itree.std_vector_lepton_pt[1]
+            eta1 = itree.std_vector_lepton_eta[0]
+            eta2 = itree.std_vector_lepton_eta[1]
 
-            met = itree.pfType1Met
-            metphi = itree.pfType1Metphi
-            #met = itree.pfmet
+            met    = itree.metPfType1
+            metphi = itree.metPfType1Phi
+            #metx   = itree.metPfType1 * cos(itree.pfType1Metphi)
+            #mety   = itree.pfType1Met * sin(itree.pfType1Metphi)
+            metsum = itree.metPfType1SumEt
+            #met    = itree.pfmet
             #metphi = itree.pfmetphi
             
-            WW = ROOT.WW(pt1, pt2, phi1, phi2, met, metphi)
+            jetpt1   = itree.std_vector_jet_pt[0]
+            jetpt2   = itree.std_vector_jet_pt[1]
+            jeteta1  = itree.std_vector_jet_eta[0]
+            jeteta2  = itree.std_vector_jet_eta[1]
+            jetphi1  = itree.std_vector_jet_phi[0]
+            jetphi2  = itree.std_vector_jet_phi[1]
+            jetmass1 = itree.std_vector_jet_mass[0]
+            jetmass2 = itree.std_vector_jet_mass[1]
+
+            WW = ROOT.WW(pt1, pt2, eta1, eta2, phi1, phi2, met, metphi, metsum, jetpt1, jetpt2, jeteta1, jeteta2, jetphi1, jetphi2, jetmass1, jetmass2)
  
-            #ptWW
-            pTWW[0]   = WW.pTWW()
-            #print "dphill = ", WW.dphill()
+            #dphillmet
+            dphillmet[0]       = WW.dphillmet()
+
+            #jetpt1_cut
+            jetpt1_cut[0]      = WW.jetpt1_cut()
+
+            #dphilljet_cut
+            dphilljet_cut[0]   = WW.dphilljet_cut()
+
+            #dphijetmet_cut
+            dphijet1met_cut[0] = WW.dphijet1met_cut()
             
+            #recoil
+            recoil[0]          = WW.recoil()
+
+            #PfMetDivSumMet
+            PfMetDivSumMet[0]  = WW.PfMetDivSumMet()
             
-            # mT2
-            #WW = ROOT.WW(pt1, pt2, phi1, phi2, met, metphi)
-            mT2[0]   = WW.mT2()
-            
-	    # mTi
-            WWi = ROOT.WW(pt1, pt2, eta1, eta2, phi1, phi2, met, metphi)
-            mTi[0]   = WWi.mTi()
-            mTe[0]   = WWi.mTe()
-            choiMass[0] = WWi.choiMass()
-            
-            # calculate HT with leptons and jets
-            HT[0] = 0.0
-            redHT[0] = 0.0
-            for iLep in range(itree.std_vector_lepton_pt.size()) :
-              if itree.std_vector_lepton_pt[iLep] > 0 :
-                HT[0] += itree.std_vector_lepton_pt[iLep]
-                if iLep >= 2 :   # exclude the first two leptons
-                  redHT[0] += itree.std_vector_lepton_pt[iLep]
-               
-            for iJet in range(itree.std_vector_jet_pt.size()) :
-              if itree.std_vector_jet_pt[iJet] > 0 :
-                HT[0] += itree.std_vector_jet_pt[iJet]
-                redHT[0] += itree.std_vector_jet_pt[iJet]
-            
-            HT[0] += met
-            #redHT[0] --> redHT doesn't use met
-            
+            #upara
+            upara[0]           = WW.upara()
+
+            #uperp
+            uperp[0]           = WW.uperp()
+
             otree.Fill()
 
         self.disconnect()
