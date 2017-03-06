217cdc5434fb29ba3990dc648217db6962806b91
NTrevisa <n.trevisani@campus.unimib.it>
2017-03-06 18:32:59 +0100

    working on lxplus

--- Tools/python/batchTools.py
+++ Tools/python/batchTools.py
@@ -287,7 +287,7 @@ def rootReadPath(inputFile):
     elif 'knu' in os.uname()[1] :
       return "dcap://cluster142.knu.ac.kr//pnfs/knu.ac.kr/data/cms" + inputFile
     else :
-        return inputFile
+        return "/eos/cms" + inputFile
     
 def remoteFileSize(inputFile):
     "Returns file size in byte for file on remote server (/store/.../*.root)"
@@ -303,7 +303,7 @@ def remoteFileSize(inputFile):
     elif "knu" in os.uname()[1]:
         return subprocess.check_output("ls -l /pnfs/knu.ac.kr/data/cms/" + inputFile + " | cut -d ' ' -f 5", shell=True)
     else :
-        return subprocess.check_output("/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select fileinfo " + inputFile + ' | grep "Size:" | cut -d ' ' -f 4', shell=True)
+        return subprocess.check_output("/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select fileinfo " + inputFile + ' | grep "Size:" | cut -d \' \' -f 4', shell=True)
 
 def batchTest():
     jobs = batchJobs('Test','Test',['Test'],['Test'],['Step','Target'])
