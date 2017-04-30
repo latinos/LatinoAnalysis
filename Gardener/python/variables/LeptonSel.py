27bbcdaa0323e5ecd4049ace312d8ab5f256b3ed
Janssen Xavier <xavier.janssen@uantwerpen.be>
2017-04-30 18:39:13 +0200

    LeptonSel: implement letpon WgStar WP

--- Gardener/python/variables/LeptonSel.py
+++ Gardener/python/variables/LeptonSel.py
@@ -48,6 +48,29 @@ class LeptonWP():
 
        return res
 
+    def isAcloseToB(self, a_Eta, a_Phi, b_Eta, b_Phi, drmax) :
+        dPhi = ROOT.TMath.Abs(b_Phi - a_Phi)
+        if dPhi > ROOT.TMath.Pi() :
+          dPhi = 2*ROOT.TMath.Pi() - dPhi
+        dR2 = (b_Eta - a_Eta) * (b_Eta - a_Eta) + dPhi * dPhi
+        if dR2 < (drmax*drmax):
+            return True
+        else:
+            return False
+
+
+    def isoConeOverlapRemoval(self,itree,iLep):
+
+        pt_to_be_removed_from_overlap = 0
+        isoConSize = 0.4
+        for jLep in xrange(len(itree.std_vector_lepton_pt)):
+          if jLep != iLep and itree.std_vector_lepton_pt[jLep]>0 :
+            if self.isAcloseToB(itree.std_vector_lepton_eta[jLep], itree.std_vector_lepton_phi[jLep],
+                                itree.std_vector_lepton_eta[iLep], itree.std_vector_lepton_phi[iLep],
+                                isoConSize) :
+                   pt_to_be_removed_from_overlap += itree.std_vector_lepton_pt[jLep]
+
+        return pt_to_be_removed_from_overlap
 
 class LeptonSel(TreeCloner):
 
@@ -60,8 +83,10 @@ class LeptonSel(TreeCloner):
     def addOptions(self,parser):
         description = self.help()
         group = optparse.OptionGroup(parser,self.label, description)
-        group.add_option('-c', '--cmssw', dest='cmssw', help='cmssw version (naming convention may change)', default='Full2016', type='string') 
-        group.add_option('-w', '--wpdic', dest='WPdic', help='WP Dictionnary', default='LatinoAnalysis/Gardener/python/variables/LeptonSel_cfg.py', type='string') 
+        group.add_option('-c', '--cmssw',   dest='cmssw', help='cmssw version (naming convention may change)', default='Full2016', type='string') 
+        group.add_option('-w', '--wpdic',   dest='WPdic', help='WP Dictionnary', default='LatinoAnalysis/Gardener/python/variables/LeptonSel_cfg.py', type='string') 
+        group.add_option('-k', '--kind' ,   dest='kindLep', help='Kind of mininal lepton to select and use for jet cleaning (2=Fake/3=WgStar/4=Loose)' , default=1)
+        group.add_option('-n', '--nMinLep', dest='nMinLep', help='Minimal number of leptons of selected kind' , default = 1 )
 
     def checkOptions(self,opts):
         
@@ -78,32 +103,48 @@ class LeptonSel(TreeCloner):
           print 'ERROR: No WP'
           exit()  
 
+        self.kindLep=int(opts.kindLep)
+        self.nMinLep=int(opts.nMinLep) 
+
         # Create Elecron WP
         self.FakeObjElectronWPs = {}
         self.TightObjElectronWPs = {}
+        self.WgStarObjElectronWPs = {}
  
         if self.cmssw in ElectronWP :
           if 'FakeObjWP' in ElectronWP[self.cmssw] : 
             for iWP in ElectronWP[self.cmssw]['FakeObjWP']  : self.FakeObjElectronWPs[iWP] = LeptonWP(self.cmssw,ElectronWP,'FakeObjWP',iWP)
           if 'TightObjWP' in ElectronWP[self.cmssw] :
             for iWP in ElectronWP[self.cmssw]['TightObjWP'] : self.TightObjElectronWPs[iWP] = LeptonWP(self.cmssw,ElectronWP,'TightObjWP',iWP)
+          if 'WgStarObjWP' in ElectronWP[self.cmssw] :
+            for iWP in ElectronWP[self.cmssw]['WgStarObjWP'] : self.WgStarObjElectronWPs[iWP] = LeptonWP(self.cmssw,ElectronWP,'WgStarObjWP',iWP)
+
 
         # Create Muon WP
         self.FakeObjMuonWPs = {}
         self.TightObjMuonWPs = {}
+        self.WgStarObjMuonWPs = {}
 
         if self.cmssw in MuonWP :
           if 'FakeObjWP' in MuonWP[self.cmssw] :
             for iWP in MuonWP[self.cmssw]['FakeObjWP']  : self.FakeObjMuonWPs[iWP] = LeptonWP(self.cmssw,MuonWP,'FakeObjWP',iWP)
           if 'TightObjWP' in MuonWP[self.cmssw] :
             for iWP in MuonWP[self.cmssw]['TightObjWP'] : self.TightObjMuonWPs[iWP] = LeptonWP(self.cmssw,MuonWP,'TightObjWP',iWP)
+          if 'WgStarObjWP' in MuonWP[self.cmssw] :
+            for iWP in MuonWP[self.cmssw]['WgStarObjWP'] : self.WgStarObjMuonWPs[iWP] = LeptonWP(self.cmssw,MuonWP,'WgStarObjWP',iWP)
 
         # What to do if more than 1 Fake Obj definition ?
         if  len(self.FakeObjMuonWPs) > 1 or len(self.FakeObjElectronWPs) > 1 :
           print 'ERROR: Can not handle more than 1 FakeObj definition'
           exit()
 
+        # What to do if more than 1 WgStar Obj definition ?
+        if  len(self.WgStarObjMuonWPs) > 1 or len(self.WgStarObjElectronWPs) > 1 :
+          print 'ERROR: Can not handle more than 1 WgStarObj definition'
+          exit()
+
     def process(self,**kwargs):
+
 
         tree  = kwargs['tree']
         input = kwargs['input']
@@ -112,7 +153,11 @@ class LeptonSel(TreeCloner):
         self.connect(tree,input)
 
         # Clone Tree
-        self.namesNewBranchesVector = ['std_vector_lepton_isLooseLepton', 'std_vector_lepton_isTightLepton']
+        self.namesNewBranchesVector = [] 
+        self.namesNewBranchesVector.append('std_vector_lepton_isLooseLepton')
+        for iWP in self.TightObjElectronWPs : self.namesNewBranchesVector.append('std_vector_electron_isTightLepton_'+iWP)
+        for iWP in self.TightObjMuonWPs     : self.namesNewBranchesVector.append('std_vector_muon_isTightLepton_'+iWP)
+        print self.namesNewBranchesVector
         self.clone(output,"")
 
         # new brances as std_vector
@@ -128,6 +173,9 @@ class LeptonSel(TreeCloner):
         # input tree and output tree
         itree     = self.itree
         otree     = self.otree
+
+        # Lepton Tags container
+        LeptonTags = {}
 
         # Loop
         nentries = self.itree.GetEntries()
@@ -148,23 +196,54 @@ class LeptonSel(TreeCloner):
             if i > 0 and i%step == 0.:
                 print i,'events processed :: ', nentries
 
-            for iLep in xrange(len(itree.std_vector_lepton_pt)) :
+            # Gep Letpon Tags: 
 
+            LeptonTags ['Loose']  = []
+            LeptonTags ['WgStar'] = []
+            for iWP in self.TightObjElectronWPs : LeptonTags['electron_'+iWP] = []
+            for iWP in self.TightObjMuonWPs     : LeptonTags['muon_'+iWP]     = [] 
+            
+            
+            for iLep in xrange(len(itree.std_vector_lepton_pt)) :
               
                # Electron
                if    abs(self.itree.std_vector_lepton_flavour[iLep]) == 11 :
-                 for iWP in self.FakeObjElectronWPs  : self.FakeObjElectronWPs[iWP].passWP(self.itree,iLep) 
-                 for iWP in self.TightObjElectronWPs : self.TightObjElectronWPs[iWP].passWP(self.itree,iLep) 
-      
+                 for iWP in self.FakeObjElectronWPs  : LeptonTags ['Loose']         .append( self.FakeObjElectronWPs[iWP].passWP(self.itree,iLep) )
+                 for iWP in self.WgStarObjElectronWPs: LeptonTags ['WgStar']        .append( self.WgStarObjElectronWPs[iWP].passWP(self.itree,iLep) )
+                 for iWP in self.TightObjElectronWPs : LeptonTags ['electron_'+iWP] .append( self.TightObjElectronWPs[iWP].passWP(self.itree,iLep) )
+                 # ... and 0 for muons
+                 for iWP in self.TightObjMuonWPs : LeptonTags['muon_'+iWP] .append( 0 )
+ 
                # Muon
                elif  abs(self.itree.std_vector_lepton_flavour[iLep]) == 13 :
-                 for iWP in self.FakeObjMuonWPs  : self.FakeObjMuonWPs[iWP].passWP(self.itree,iLep)
-                 for iWP in self.TightObjMuonWPs : self.TightObjMuonWPs[iWP].passWP(self.itree,iLep)
+                 for iWP in self.FakeObjMuonWPs  : LeptonTags ['Loose']     .append( self.FakeObjMuonWPs[iWP].passWP(self.itree,iLep) )
+                 for iWP in self.WgStarObjMuonWPs: LeptonTags ['WgStar']    .append( self.WgStarObjMuonWPs[iWP].passWP(self.itree,iLep) )
+                 for iWP in self.TightObjMuonWPs : LeptonTags ['muon_'+iWP] .append( self.TightObjMuonWPs[iWP].passWP(self.itree,iLep) )
+                 # ... and 0 for electrons
+                 for iWP in self.TightObjElectronWPs : LeptonTags['electron_'+iWP] .append( 0 )
+ 
+               # ... and 0 if not a lepton
+               else:  
+                 for iWP in self.FakeObjMuonWPs       : LeptonTags ['Loose']         .append( 0 )
+                 for iWP in self.WgStarObjMuonWPs     : LeptonTags ['WgStar']        .append( 0 )
+                 for iWP in self.TightObjElectronWPs  : LeptonTags ['electron_'+iWP] .append( 0 )
+                 for iWP in self.TightObjMuonWPs      : LeptonTags ['muon_'+iWP]     .append( 0 ) 
 
-#             print iLep
+            print LeptonTags
 
-#             LooseTag = 0
-#             TightTag = 0 
+            # At least nLeptons of kindLep or throw away the event
+            if   self.kindLep == 2 : CleanTag = 'Loose'
+            elif self.kindLep == 3 : CleanTag = 'WgStar'  
+            else:
+              print 'ERROR: kindLep unavailable : ',self.kindLep
+              exit() 
+            if LeptonTags [CleanTag].count(1) >= self.nMinLep :   
+
+
+              # Lepton cleaning
+               
+              # jet cleaning
+
 #
 #             for bname, bvector in self.newBranchesVector.iteritems():
 #                bvector.clear()
@@ -173,8 +252,9 @@ class LeptonSel(TreeCloner):
 #                if bname == 'std_vector_lepton_isTightLepton' :
 #                  bvector.push_back (TightTag)
 
-       
-            otree.Fill()
+  
+              savedentries+=1     
+              otree.Fill()
 
         self.disconnect()
         print '- Eventloop completed'
--- Gardener/python/variables/LeptonSel_cfg.py
+++ Gardener/python/variables/LeptonSel_cfg.py
@@ -68,6 +68,37 @@ ElectronWP['Full2016'] = {
                              } ,
 
                  } ,
+
+ # ------------ 
+ 'WgStarObjWP' : {
+
+         'cut_WP_Tight80X' : {
+                                # Common cuts
+                                'True' :
+                                  [
+                                    'abs(itree.std_vector_lepton_eta[]) < 2.5' ,
+                                    'itree.std_vector_lepton_eleIdHLT[]',
+                                    'itree.std_vector_lepton_eleIdTight[]',
+                                    'itree.std_vector_electron_expectedMissingInnerHits[] < 1',
+                                    'itree.std_vector_electron_tripleChargeAgreement[]',
+                                  ] ,
+                                # Barrel
+                                'abs(itree.std_vector_lepton_eta[]) <= 1.479' :
+                                  [
+                                    'itree.std_vector_lepton_d0[] < 0.05' ,
+                                    'itree.std_vector_lepton_dz[] < 0.1'  ,
+                                  ] ,
+                                # EndCap
+                                'abs(itree.std_vector_lepton_eta[]) > 1.479' :
+                                  [
+                                    'itree.std_vector_lepton_d0[] < 0.1' ,
+                                    'itree.std_vector_lepton_dz[] < 0.2'  ,
+                                  ] ,
+                             } ,
+
+                 } ,
+
+
 }
 
 
@@ -81,8 +112,8 @@ MuonWP['Full2016'] = {
 
  'Variables' : {
 
-        'muonIso' : 'max( itree.std_vector_lepton_photonIso[] + itree.std_vector_lepton_neutralHadronIso[] - 0.5 * itree.std_vector_lepton_sumPUPt[] , 0 )'
-
+        'muonIso' : 'max( itree.std_vector_lepton_photonIso[] + itree.std_vector_lepton_neutralHadronIso[] - 0.5 * itree.std_vector_lepton_sumPUPt[] , 0 )' ,
+        'pt_to_be_removed_from_overlap' : 'self.isoConeOverlapRemoval(itree,iLep)'
 
                }, 
 
@@ -145,5 +176,32 @@ MuonWP['Full2016'] = {
                        } ,
 
                  } ,
+
+ # -------------
+ 'WgStarObjWP' : {
+     'cut_Tight80x' : { 
+                                # Common cuts
+                                'True' :
+                                 [
+                                   'abs(itree.std_vector_lepton_eta[]) < 2.4' ,
+                                   'itree.std_vector_lepton_isTightMuon[] == 1' ,
+                                   'abs(itree.std_vector_lepton_dz[]) < 0.1' ,
+                                   '(itree.std_vector_lepton_chargedHadronIso[] + muonIso - pt_to_be_removed_from_overlap ) / itree.std_vector_lepton_pt[] < 0.15',
+                                   'itree.std_vector_lepton_trackIso[]/itree.std_vector_lepton_pt[] < 0.4' ,
+                                 ] ,
+                                 # dxy for pT < 20 GeV
+                                 'itree.std_vector_lepton_pt[] <= 20.0' :
+                                 [
+                                    'abs(itree.std_vector_lepton_d0[]) < 0.01 ' ,
+                                 ] ,
+                                 # dxy for pT > 20 GeV
+                                 'itree.std_vector_lepton_pt[] > 20.0' :
+                                 [
+                                    'abs(itree.std_vector_lepton_d0[]) < 0.02 ' ,
+                                 ] ,
+
+                       } ,
+ 
+                 }, 
 
 }
