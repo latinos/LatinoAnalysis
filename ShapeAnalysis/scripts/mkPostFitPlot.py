432e005706b6fbd6bd098a087963064515592645
amassiro <massironi.andrea@gmail.com>
2017-11-28 10:03:24 +0100

    propagate signal in post fit plot

--- ShapeAnalysis/scripts/mkPostFitPlot.py
+++ ShapeAnalysis/scripts/mkPostFitPlot.py
@@ -281,6 +281,12 @@ if __name__ == '__main__':
        print " Please provide the datacard structure "
        #exit ()
 
+    if os.path.exists(opt.structureFile) :
+      handle = open(opt.structureFile,'r')
+      exec(handle)
+      handle.close()
+
+
     factory._structure = structure
     
     factory._inputFile = opt.inputFile
