f41066451ad9182eb3b2748b1c2e1f3ea4211252
Janssen Xavier <xavier.janssen@uantwerpen.be>
2018-03-27 04:58:55 +0200

    Module CFG

---
+++ NanoGardener/python/framework/PostProc_cfg.py
@@ -0,0 +1,84 @@
+
+
+PostProcCfg = {
+
+# ------------------------------------------------ CHAINS ----------------------------------------------------
+
+  'TestChain' : {
+                  'isChain'    : True  ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : True  ,
+                  'selection'  : 'selection = "nElectron>0 && nMuon>0 && Electron_pt[0]>20 && Muon_pt[0]>20 && nJet>1 && Jet_pt[0]>30 && Jet_pt[1]>30"' , 
+                  'subTargets' : ['lepMergerHWW','baseW','formulasMC','l2Kin'], 
+                }
+
+# ------------------------------------------------ MODULES ---------------------------------------------------
+
+  
+# ------- MODULES: Object Handling
+
+  'lepMergerHWW' : { 
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : True  ,
+                  'import'     : 'PhysicsTools.NanoAODTools.postprocessing.modules.common.collectionMerger' ,
+                  'declare'    : 'lepMergerHWW = lambda : collectionMerger( input  = ["Electron","Muon"], output = "Lepton", reverse = True' ,
+                  'module'     : 'lepMergerHWW()' ,
+               },  
+
+ 
+
+# ------- MODULES: MC Weights
+
+  'baseW'    : {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : False  ,
+                  'import'     : 'LatinoAnalysis.NanoGardener.modules.Grafter' ,
+                  'module'     : 'Grafter(["baseW/F=1."])',
+               },  
+
+  'wwNLL'    : {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : False  ,
+                  'import'     : 'LatinoAnalysis.NanoGardener.modules.wwNLLcorrectionWeightProducer' ,
+                  'module'     : 'wwNLLcorrectionWeightProducer()',  
+
+# ------- MODULES: Kinematic
+
+  'l2Kin'    : {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : True  ,
+                  'import'     : 'LatinoAnalysis.NanoGardener.modules.l2KinProducer' ,
+                  'module'     : 'l2KinProducer()' ,
+               },  
+
+  'l3Kin'    : {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : True  ,
+                  'import'     : 'LatinoAnalysis.NanoGardener.modules.l3KinProducer' ,
+                  'module'     : 'l3KinProducer()' ,
+               },  
+
+  'l4Kin'    : {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : True  ,
+                  'import'     : 'LatinoAnalysis.NanoGardener.modules.l4KinProducer' ,
+                  'module'     : 'l4KinProducer()' ,
+               },  
+
+# ------- MODULES: Adding Formulas
+
+  'formulasMC' : {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : False  ,
+                  'import'     : 'LatinoAnalysis.NanoGardener.modules.GenericFormulaAdder' ,
+                  'module'     : 'GenericFormulaAdder(\'data/formulasToAdd_MC.py\')' ,
+                 }
+
+} 
