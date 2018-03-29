f623c6ab78586a713baf0e24c256bac455696296
Janssen Xavier <xavier.janssen@uantwerpen.be>
2018-03-29 05:58:26 +0200

    First Nano PostProc version with Batch

---
+++ NanoGardener/python/framework/Sites_cfg.py
@@ -0,0 +1,25 @@
+
+Sites = {
+
+  'iihe' : { 
+              'lsCmd'       : 'ls' ,
+              'mkDir'       : False , 
+              'xrootdPath'  : 'dcap://maite.iihe.ac.be/' ,
+              'treeBaseDir' : '/pnfs/iihe/cms/store/user/xjanssen/HWWNano/' ,
+           } ,
+
+  'cern' : {
+              'lsCmd'       : 'ls' ,
+              'mkDir'       : True ,
+              'xrootdPath'  : 'root://eoscms.cern.ch/' ,
+              'treeBaseDir' : '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/' ,
+           } ,
+
+  'sdfarm' : {
+              'lsCmd'       : 'ls' ,
+              'mkDir'       : True ,
+              'xrootdPath'  : 'root://eoscms.cern.ch/' ,
+              'treeBaseDir' : '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/' ,
+             }
+
+}
