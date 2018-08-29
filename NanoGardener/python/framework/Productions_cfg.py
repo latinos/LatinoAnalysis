fda026256c874eb4125516c65aaa34a4dfc05fb8
Senne Van Putte <senne.vanputte@student.uantwerpen.be>
2018-08-29 15:17:43 +0200

    Full2017 preparations

--- NanoGardener/python/framework/Productions_cfg.py
+++ NanoGardener/python/framework/Productions_cfg.py
@@ -24,6 +24,20 @@ Productions = {
                          'isData'  : False ,
                          'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/fall17_nAOD_v1.py' ,
                          'cmssw'   : 'Study2017' ,
-                   }                
+                   },               
+
+
+   'Run2017_nAOD_v1_Full2017':  {
+                         'isData'  : True ,
+                         'jsonFile'   : '"%s/src/LatinoAnalysis/NanoGardener/python/data/certification/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt"  % os.environ["CMSSW_BASE"]',
+                         'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2017_nAOD_v1.py' ,
+                         'cmssw'   : 'Full2017' ,
+                   },
+
+   'Fall2017_nAOD_v1_Full2017':  {
+                         'isData'  : False ,
+                         'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/fall17_nAOD_v1.py' ,
+                         'cmssw'   : 'Full2017' ,
+                   },                
 
 }
