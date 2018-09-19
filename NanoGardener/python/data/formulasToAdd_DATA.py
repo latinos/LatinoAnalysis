12cb6991039ec9830dd789950ea624627488c1af
Janssen Xavier <xavier.janssen@uantwerpen.be>
2018-09-19 15:20:20 +0200

    Add DATA forrmula for SS VBS

--- NanoGardener/python/data/formulasToAdd_DATA.py
+++ NanoGardener/python/data/formulasToAdd_DATA.py
@@ -22,6 +22,9 @@ formulas['METFilter_DATA'] = METFilter_DATA
 formulas['LepCut2l'] = '(event.nLepton>=2 and (event.Lepton_isTightElectron_mvaFall17Iso_WP90[0]>0.5 or event.Lepton_isTightMuon_cut_Tight_HWWW[0]>0.5) and \
                                               (event.Lepton_isTightElectron_mvaFall17Iso_WP90[1]>0.5 or event.Lepton_isTightMuon_cut_Tight_HWWW[1]>0.5) )' 
 
+formulas['LepCut2lSS'] = '(event.nLepton>=2 and (event.Lepton_isTightElectron_mvaFall17Iso_WP90_SS[0]>0.5 or event.Lepton_isTightMuon_cut_Tight_HWWW[0]>0.5) and \
+                                              (event.Lepton_isTightElectron_mvaFall17Iso_WP90_SS[1]>0.5 or event.Lepton_isTightMuon_cut_Tight_HWWW[1]>0.5) )' 
+
 formulas['LepCut3l'] = '(event.nLepton>=3 and (event.Lepton_isTightElectron_mvaFall17Iso_WP90[0]>0.5 or event.Lepton_isTightMuon_cut_Tight_HWWW[0]>0.5) and \
                                               (event.Lepton_isTightElectron_mvaFall17Iso_WP90[1]>0.5 or event.Lepton_isTightMuon_cut_Tight_HWWW[1]>0.5) and \
                                               (event.Lepton_isTightElectron_mvaFall17Iso_WP90[2]>0.5 or event.Lepton_isTightMuon_cut_Tight_HWWW[2]>0.5) )'
