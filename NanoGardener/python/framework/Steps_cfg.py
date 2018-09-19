eed50b73bf7cd8b2f4f2c16b4e1ef70cb7f69055
Piergiulio <piergiulio.lenzi@cern.ch>
2018-09-19 15:33:21 +0200

    chain for MCformula

--- NanoGardener/python/framework/Steps_cfg.py
+++ NanoGardener/python/framework/Steps_cfg.py
@@ -21,7 +21,14 @@ Steps = {
                   'do4Data'    : False ,
                   'selection'  : '"((nElectron+nMuon)>0)"' ,
                   'subTargets' : ['leptonMaker','lepSel', 'puW2017', 'l2Kin', 'l3Kin', 'l4Kin', 'btagPerJet2017', 'btagPerEvent'],
-                }, 
+                },
+
+  'MCformulas': {
+                  'isChain'    : True  ,
+                  'do4MC'      : True ,
+                  'do4Data'    : False  ,           
+                  'subTargets' : ['baseW','trigMC','formulasMC'],
+                },             
 
 ## ------- DATA:
     
