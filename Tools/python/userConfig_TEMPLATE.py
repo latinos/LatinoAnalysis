6675926675d2ce74905b8db810736612ce8a9e38
NTrevisa <n.trevisani@campus.unimib.it>
2018-12-13 16:55:35 +0100

    Âcondor

--- Tools/python/userConfig_TEMPLATE.py
+++ Tools/python/userConfig_TEMPLATE.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
-baseDir  = '/afs/cern.ch/user/x/xjanssen/cms/HWW2015/'
-jobDir   = baseDir+'jobs/'
-workDir  = baseDir+'workspace/'
+baseDir   = '/afs/cern.ch/user/x/xjanssen/cms/HWW2015/'
+jobDir    = baseDir+'jobs/'
+workDir   = baseDir+'workspace/'
+batchType = 'condor'
