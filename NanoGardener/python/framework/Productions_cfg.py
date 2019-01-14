648f2b5cab28cc378ce5bc13a159671556302cbc
xjanssen <xavier.janssen@cern.ch>
2019-01-14 22:50:26 +0100

    Adding Run2017_102X_nAODv4

--- NanoGardener/python/framework/Productions_cfg.py
+++ NanoGardener/python/framework/Productions_cfg.py
@@ -71,18 +71,18 @@ Productions = {
 ################################### 102X nAODv3 ######################################
 
  # -------- 2016 DATA 102X nAODv3
- 'Run2016_102X_nAODv3_Full2016v3': {
+ 'Run2016_102X_nAODv4_Full2016v2': {
                        'isData'  : True ,
                        'jsonFile'   : '"%s/src/LatinoAnalysis/NanoGardener/python/data/certification/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt"  % os.environ["CMSSW_BASE"]',
-                       'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2016_102X_nAODv3.py' ,
-                       'cmssw'   : 'Full2016v3',
+                       'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2016_102X_nAODv4.py' ,
+                       'cmssw'   : 'Full2016v2',
                    }, 
 
  # -------- 2017 DATA 102X nAODv3 (TODO: samples/Run2017_102X_nAODv3.py)
- 'Run2017_102X_nAODv3_Full2017v3': {
+ 'Run2017_102X_nAODv4_Full2017v3': {
                        'isData'  : True ,
                        'jsonFile'   : '"%s/src/LatinoAnalysis/NanoGardener/python/data/certification/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt"  % os.environ["CMSSW_BASE"]',
-                       'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2017_102X_nAODv3.py' ,
+                       'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2017_102X_nAODv4.py' ,
                        'cmssw'   : 'Full2017v3',
                    },
 
