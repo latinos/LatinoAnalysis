bcd49f3896bf24d9a6474d49a388ad568a812e0d
xjanssen <xavier.janssen@cern.ch>
2019-02-07 18:55:49 +0100

    up

--- NanoGardener/python/framework/samples/Run2018_102X_nAODv4_14Sep2018.py
+++ NanoGardener/python/framework/samples/Run2018_102X_nAODv4_14Sep2018.py
@@ -10,16 +10,6 @@ Samples["DoubleMuon_Run2018C-14Sep2018_ver2-v1"] = {'nanoAOD': '/DoubleMuon/Run2
 Samples["DoubleMuon_Run2018C-14Sep2018_ver3-v1"] = {'nanoAOD': '/DoubleMuon/Run2018C-14Sep2018_ver3-v1/NANOAOD'}
 Samples["DoubleMuon_Run2018D-14Sep2018_ver2-v1"] = {'nanoAOD': '/DoubleMuon/Run2018D-14Sep2018_ver2-v1/NANOAOD'}
 
-#Samples["DoubleMuonLowMass_Run2018A-14Sep2018_ver1-v1"] = {'nanoAOD': '/DoubleMuonLowMass/Run2018A-14Sep2018_ver1-v1/NANOAOD'}
-#Samples["DoubleMuonLowMass_Run2018A-14Sep2018_ver2-v1"] = {'nanoAOD': '/DoubleMuonLowMass/Run2018A-14Sep2018_ver2-v1/NANOAOD'}
-#Samples["DoubleMuonLowMass_Run2018A-14Sep2018_ver3-v1"] = {'nanoAOD': '/DoubleMuonLowMass/Run2018A-14Sep2018_ver3-v1/NANOAOD'}
-#Samples["DoubleMuonLowMass_Run2018B-14Sep2018_ver1-v1"] = {'nanoAOD': '/DoubleMuonLowMass/Run2018B-14Sep2018_ver1-v1/NANOAOD'}
-#Samples["DoubleMuonLowMass_Run2018B-14Sep2018_ver2-v1"] = {'nanoAOD': '/DoubleMuonLowMass/Run2018B-14Sep2018_ver2-v1/NANOAOD'}
-#Samples["DoubleMuonLowMass_Run2018C-14Sep2018_ver1-v1"] = {'nanoAOD': '/DoubleMuonLowMass/Run2018C-14Sep2018_ver1-v1/NANOAOD'}
-#Samples["DoubleMuonLowMass_Run2018C-14Sep2018_ver2-v1"] = {'nanoAOD': '/DoubleMuonLowMass/Run2018C-14Sep2018_ver2-v1/NANOAOD'}
-#Samples["DoubleMuonLowMass_Run2018C-14Sep2018_ver3-v1"] = {'nanoAOD': '/DoubleMuonLowMass/Run2018C-14Sep2018_ver3-v1/NANOAOD'}
-#Samples["DoubleMuonLowMass_Run2018D-14Sep2018_ver2-v1"] = {'nanoAOD': '/DoubleMuonLowMass/Run2018D-14Sep2018_ver2-v1/NANOAOD'}
-
 Samples["EGamma_Run2018A-14Sep2018_ver1-v1"] = {'nanoAOD': '/EGamma/Run2018A-14Sep2018_ver1-v1/NANOAOD'}
 Samples["EGamma_Run2018A-14Sep2018_ver2-v1"] = {'nanoAOD': '/EGamma/Run2018A-14Sep2018_ver2-v1/NANOAOD'}
 Samples["EGamma_Run2018A-14Sep2018_ver3-v1"] = {'nanoAOD': '/EGamma/Run2018A-14Sep2018_ver3-v1/NANOAOD'}
@@ -30,8 +20,8 @@ Samples["EGamma_Run2018C-14Sep2018_ver2-v1"] = {'nanoAOD': '/EGamma/Run2018C-14S
 Samples["EGamma_Run2018C-14Sep2018_ver3-v1"] = {'nanoAOD': '/EGamma/Run2018C-14Sep2018_ver3-v1/NANOAOD'}
 Samples["EGamma_Run2018D-14Sep2018_ver2-v1"] = {'nanoAOD': '/EGamma/Run2018D-14Sep2018_ver2-v1/NANOAOD'}
 
-Samples["SingleMuon_Run2018A-14Sep2018_ver1-v1"] = {'nanoAOD': '/SingleMuon/Run2018A-14Sep2018_06Jun2018-v1/NANOAOD'}
-Samples["SingleMuon_Run2018A-14Sep2018_ver2-v1"] = {'nanoAOD': '/SingleMuon/Run2018A-14Sep2018_ver2-v1/NANOAOD'}
+# Here there was a restart of prod covering ver1 and ver2
+Samples["SingleMuon_Run2018A-14Sep2018_06Jun2018-v1"] = {'nanoAOD': '/SingleMuon/Run2018A-14Sep2018_06Jun2018-v1/NANOAOD'}
 Samples["SingleMuon_Run2018A-14Sep2018_ver3-v1"] = {'nanoAOD': '/SingleMuon/Run2018A-14Sep2018_ver3-v1/NANOAOD'}
 Samples["SingleMuon_Run2018B-14Sep2018_ver1-v1"] = {'nanoAOD': '/SingleMuon/Run2018B-14Sep2018_ver1-v1/NANOAOD'}
 Samples["SingleMuon_Run2018B-14Sep2018_ver2-v1"] = {'nanoAOD': '/SingleMuon/Run2018B-14Sep2018_ver2-v1/NANOAOD'}
