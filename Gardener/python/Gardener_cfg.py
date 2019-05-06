8ea1739bcfc2ff3a4be5dac7eb76b1b5e29abb9c
Arianna <arianna.chiapparini@gmail.com>
2019-05-06 15:36:11 +0200

    Jet pairing for HH gardener modules added

--- Gardener/python/Gardener_cfg.py
+++ Gardener/python/Gardener_cfg.py
@@ -5254,12 +5254,27 @@ Steps= {
                   'command'    : 'gardener.py JetPairingGenVBS --radius 0.8 --ptminjet 20.0'
   },
 
+  'HHPairingGenAndVars' :{
+                  'isChain'    : True ,
+                  'do4MC'      : True ,
+                  'do4Data'    : True,
+                  'subTargets' : ['gr4JetsSkim', 'JetPairingGenHH','JetPairingHH']
+  },
+
   'JetPairingVBS' :{
                   'isChain'    : False ,
                   'do4MC'      : True ,
                   'do4Data'    : True,
                   'command'    : 'gardener.py JetPairingVBS --ptminjet 20.0'
   },
+
+  'JetPairingGenVBS' :{
+                  'isChain'    : False ,
+                  'do4MC'      : True ,
+                  'do4Data'    : False,
+                  'command'    : 'gardener.py JetPairingGenHH --radius 0.8 --ptminjet 20.0 --bWP M -m 0'
+  },
+
 
   'JetPairingHH' :{
                   'isChain'    : False ,
