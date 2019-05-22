da9a154ee7bed6e900675eb9bb6c88ec2cfd50ea
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2019-05-22 14:12:32 +0200

    Fix error in hadd step

--- ShapeAnalysis/scripts/mkShapesMulti.py
+++ ShapeAnalysis/scripts/mkShapesMulti.py
@@ -497,7 +497,7 @@ if __name__ == '__main__':
 
       if not opt.doNotCleanup:
         for fname in fileList:
-          os.unlink(fname)
+          os.unlink(opt.outputDir + '/' + fname)
 
     elif opt.doHadd != 0 or opt.redoStat != 0:
       ## Fix the MC stat nuisances that are not treated correctly in case of AsMuchAsPossible option
