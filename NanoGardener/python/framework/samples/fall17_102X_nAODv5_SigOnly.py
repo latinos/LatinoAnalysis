1e3dc4be09b674b5fd0da4668a0199b10f95c996
jhchoi <soarnsoar@gmail.com>
2019-08-06 05:38:29 +0900

    Delete fall17_102X_nAODv5_SigOnly.py

--- NanoGardener/python/framework/samples/fall17_102X_nAODv5_SigOnly.py
+++
@@ -1,5 +0,0 @@
-
-Samples = {}
-
-
-Samples['GluGluHToWWTo2L2NuPowheg_M125'] = {'nanoAOD' :'/GluGluHToWWTo2L2Nu_M125_13TeV_powheg2_JHUGenV714_pythia8/RunIIFall17NanoAODv5-PU2017_12Apr2018_Nano1June2019_102X_mc2017_realistic_v7-v1/NANOAODSIM'}
