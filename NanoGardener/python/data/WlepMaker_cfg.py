187b470e3d3f5c4103dee94550d190d6de367573
your name your last name <soarnsoar@gmail.com>
2019-08-07 14:12:38 +0900

    Add WlepMaker

---
+++ NanoGardener/python/data/WlepMaker_cfg.py
@@ -0,0 +1,20 @@
+#--jhchoi 2019.June.20th
+
+Wlep_br = {
+               'F': [
+                     'Wlep_pt',
+                     'Wlep_eta',
+                     'Wlep_phi',
+                     'Wlep_mass',
+                     'Wlep_px',
+                     'Wlep_py',
+                     'Wlep_pz',
+                     'Wlep_E',
+
+                   
+                     ],
+
+
+              }
+
+Wlep_var = ['pt', 'eta', 'phi', 'mass', 'px', 'py', 'pz', 'E']
