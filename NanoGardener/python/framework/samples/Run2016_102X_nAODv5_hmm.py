967fce03f9c0030bebd9f6e36b00064447e4fdc6
NTrevisa <n.trevisani@campus.unimib.it>
2019-08-09 12:56:45 +0200

    data samples

---
+++ NanoGardener/python/framework/samples/Run2016_102X_nAODv5_hmm.py
@@ -0,0 +1,21 @@
+Samples = {}
+
+##Samples["SingleMuon_Run2016B-Nano1June2019_ver1-v1"]      = {'nanoAOD': '/SingleMuon/Run2016B_ver1-Nano1June2019_ver1-v1/NANOAOD'} 
+Samples["SingleMuon_Run2016B-Nano1June2019_ver2-v1"] = {'nanoAOD': '/SingleMuon/Run2016B_ver2-Nano1June2019_ver2-v1/NANOAOD'} 
+Samples["SingleMuon_Run2016C-Nano1June2019-v1"]      = {'nanoAOD': '/SingleMuon/Run2016C-Nano1June2019-v1/NANOAOD'} 
+Samples["SingleMuon_Run2016D-Nano1June2019-v1"]      = {'nanoAOD': '/SingleMuon/Run2016D-Nano1June2019-v1/NANOAOD'} 
+Samples["SingleMuon_Run2016E-Nano1June2019-v1"]      = {'nanoAOD': '/SingleMuon/Run2016E-Nano1June2019-v1/NANOAOD'} 
+Samples["SingleMuon_Run2016F-Nano1June2019-v1"]      = {'nanoAOD': '/SingleMuon/Run2016F-Nano1June2019-v1/NANOAOD'} 
+Samples["SingleMuon_Run2016G-Nano1June2019-v1"]      = {'nanoAOD': '/SingleMuon/Run2016G-Nano1June2019-v1/NANOAOD'} 
+Samples["SingleMuon_Run2016H-Nano1June2019-v1"]      = {'nanoAOD': '/SingleMuon/Run2016H-Nano1June2019-v1/NANOAOD'} 
+
+
+
+# #Samples["SingleMuon_Run2016B-Nano14Dec2018_ver1-v1"] = {'nanoAOD': '/SingleMuon/Run2016B_ver1-Nano14Dec2018_ver1-v1/NANOAOD' }
+# Samples["SingleMuon_Run2016B-Nano14Dec2018_ver2-v1"] = {'nanoAOD': '/SingleMuon/Run2016B_ver2-Nano14Dec2018_ver2-v1/NANOAOD' }
+# Samples["SingleMuon_Run2016C-Nano14Dec2018-v1"]      = {'nanoAOD': '/SingleMuon/Run2016C-Nano14Dec2018-v1/NANOAOD' }
+# Samples["SingleMuon_Run2016D-Nano14Dec2018-v1"]      = {'nanoAOD': '/SingleMuon/Run2016D-Nano14Dec2018-v1/NANOAOD' }
+# Samples["SingleMuon_Run2016E-Nano14Dec2018-v1"]      = {'nanoAOD': '/SingleMuon/Run2016E-Nano14Dec2018-v1/NANOAOD' }
+# Samples["SingleMuon_Run2016F-Nano14Dec2018-v1"]      = {'nanoAOD': '/SingleMuon/Run2016F-Nano14Dec2018-v1/NANOAOD' }
+# Samples["SingleMuon_Run2016G-Nano14Dec2018-v1"]      = {'nanoAOD': '/SingleMuon/Run2016G-Nano14Dec2018-v1/NANOAOD' }
+# Samples["SingleMuon_Run2016H-Nano14Dec2018-v1"]      = {'nanoAOD': '/SingleMuon/Run2016H-Nano14Dec2018-v1/NANOAOD' }
