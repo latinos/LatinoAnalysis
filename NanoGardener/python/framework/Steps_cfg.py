c6b467a8ec2d31aeb1082004df60cf3634d1cd24
NTrevisa <n.trevisani@campus.unimib.it>
2019-08-20 18:28:52 +0200

    rochester corrections for data

--- NanoGardener/python/framework/Steps_cfg.py
+++ NanoGardener/python/framework/Steps_cfg.py
@@ -301,7 +301,7 @@ Steps = {
                   'do4MC'      : False  ,
                   'do4Data'    : True ,
                   'selection'  : '"((nMuon)>1)"' ,
-                  'subTargets' : ['leptonMaker','TriggerObjectMatch','lepSel','jetSelHmm2016','l2Kin','formulasDATAhmm'],
+                  'subTargets' : ['leptonMaker','TriggerObjectMatch','lepSel','jetSelHmm2016','rochesterDATA','l2Kin','formulasDATAhmm'],
                 },
 
   'DATAl1loose2017': {
