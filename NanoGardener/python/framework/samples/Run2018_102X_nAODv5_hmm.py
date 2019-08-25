7680bcab2880e8a23d8101891f5cd0824a79e783
NTrevisa <n.trevisani@campus.unimib.it>
2019-08-25 10:46:07 +0200

    data 2018 hmm

---
+++ NanoGardener/python/framework/samples/Run2018_102X_nAODv5_hmm.py
@@ -0,0 +1,6 @@
+Samples = {}
+
+Samples["SingleMuon_Run2018A-Nano1June2019-v1"]      = {'nanoAOD': '/SingleMuon/Run2018A-Nano1June2019-v1/NANOAOD'}
+Samples["SingleMuon_Run2018B-Nano1June2019-v1"]      = {'nanoAOD': '/SingleMuon/Run2018B-Nano1June2019-v1/NANOAOD'}
+Samples["SingleMuon_Run2018C-Nano1June2019-v1"]      = {'nanoAOD': '/SingleMuon/Run2018C-Nano1June2019-v1/NANOAOD'}
+Samples["SingleMuon_Run2018D-Nano1June2019_ver2-v1"] = {'nanoAOD': '/SingleMuon/Run2018D-Nano1June2019-v1/NANOAOD'}
