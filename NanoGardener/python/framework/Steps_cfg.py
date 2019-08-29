0f1495c3b630e0cf6e666cbffc23c6587670cb59
Yutaro <yutaro.iiyama@cern.ch>
2019-08-29 17:52:48 +0200

    actually supporting branchsel in PostProcMaker and adding a few steps for gen-only studies

--- NanoGardener/python/framework/Steps_cfg.py
+++ NanoGardener/python/framework/Steps_cfg.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+
 ## --------------------------------- Some predefined sequence Chains -----------------------------------------
 
 
@@ -231,6 +233,14 @@ Steps = {
                                      'rochesterMC','trigMC','LeptonSF','puW','l2Kin', 'l3Kin', 'l4Kin','formulasMC'],
                 },
 
+  'MCGenOnly': {
+                  'isChain'    : True  ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : False ,
+                  'subTargets' : ['PromptParticlesGenVars','GenVar', 'HiggsGenVars', 'ggHTheoryUncertainty', 'DressedLeptons',
+                                  'baseW'],
+                  'outputbranchsel': os.getenv('CMSSW_BASE') + '/src/LatinoAnalysis/NanoGardener/python/data/MCGenOnly_outputbranches.txt'
+               },
 
 ## ------- WgStar MC:
 
@@ -521,7 +531,7 @@ Steps = {
                    'declare'    : 'ggHUncertaintyProducer = lambda : GGHUncertaintyProducer()',
                    'module'     : 'ggHUncertaintyProducer()',
                    'onlySample' : [
-                                  'GluGluHToWWTo2L2NuPowheg_M125_PrivateNano'
+                                  'GluGluHToWWTo2L2NuPowheg_M125_PrivateNano', 'GluGluHToWWTo2L2NuPowheg_M125'
                                   ]
                   },    
 
@@ -591,6 +601,15 @@ Steps = {
                                   'GluGluHToWWToLNuQQ_M115', 'GluGluHToWWToLNuQQ_M120', 'GluGluHToWWToLNuQQ_M124', 'GluGluHToWWToLNuQQ_M125', 'GluGluHToWWToLNuQQ_M126', 'GluGluHToWWToLNuQQ_M130', 'GluGluHToWWToLNuQQ_M135', 'GluGluHToWWToLNuQQ_M140', 'GluGluHToWWToLNuQQ_M145', 'GluGluHToWWToLNuQQ_M150', 'GluGluHToWWToLNuQQ_M155', 'GluGluHToWWToLNuQQ_M160', 'GluGluHToWWToLNuQQ_M165', 'GluGluHToWWToLNuQQ_M170', 'GluGluHToWWToLNuQQ_M175', 'GluGluHToWWToLNuQQ_M180', 'GluGluHToWWToLNuQQ_M190', 'GluGluHToWWToLNuQQ_M200', 'GluGluHToWWToLNuQQ_M210', 'GluGluHToWWToLNuQQ_M230', 'GluGluHToWWToLNuQQ_M250', 'GluGluHToWWToLNuQQ_M270', 'GluGluHToWWToLNuQQ_M300', 'GluGluHToWWToLNuQQ_M350', 'GluGluHToWWToLNuQQ_M400', 'GluGluHToWWToLNuQQ_M450', 'GluGluHToWWToLNuQQ_M500', 'GluGluHToWWToLNuQQ_M550', 'GluGluHToWWToLNuQQ_M600', 'GluGluHToWWToLNuQQ_M650', 'GluGluHToWWToLNuQQ_M700', 'GluGluHToWWToLNuQQ_M750', 'GluGluHToWWToLNuQQ_M800', 'GluGluHToWWToLNuQQ_M850', 'GluGluHToWWToLNuQQ_M900', 'GluGluHToWWToLNuQQ_M1000', 'GluGluHToWWToLNuQQ_M1500', 'GluGluHToWWToLNuQQ_M2000', 'GluGluHToWWToLNuQQ_M2500', 'GluGluHToWWToLNuQQ_M3000', 'GluGluHToWWToLNuQQ_M4000', 'GluGluHToWWToLNuQQ_M5000',
                                   'VBFHToWWToLNuQQ_M115', 'VBFHToWWToLNuQQ_M120', 'VBFHToWWToLNuQQ_M124', 'VBFHToWWToLNuQQ_M125', 'VBFHToWWToLNuQQ_M126', 'VBFHToWWToLNuQQ_M130', 'VBFHToWWToLNuQQ_M135', 'VBFHToWWToLNuQQ_M140', 'VBFHToWWToLNuQQ_M145', 'VBFHToWWToLNuQQ_M150', 'VBFHToWWToLNuQQ_M155', 'VBFHToWWToLNuQQ_M160', 'VBFHToWWToLNuQQ_M165', 'VBFHToWWToLNuQQ_M170', 'VBFHToWWToLNuQQ_M175', 'VBFHToWWToLNuQQ_M180', 'VBFHToWWToLNuQQ_M190', 'VBFHToWWToLNuQQ_M200', 'VBFHToWWToLNuQQ_M210', 'VBFHToWWToLNuQQ_M230', 'VBFHToWWToLNuQQ_M250', 'VBFHToWWToLNuQQ_M270', 'VBFHToWWToLNuQQ_M300', 'VBFHToWWToLNuQQ_M350', 'VBFHToWWToLNuQQ_M400', 'VBFHToWWToLNuQQ_M450', 'VBFHToWWToLNuQQ_M500', 'VBFHToWWToLNuQQ_M550', 'VBFHToWWToLNuQQ_M600', 'VBFHToWWToLNuQQ_M650', 'VBFHToWWToLNuQQ_M700', 'VBFHToWWToLNuQQ_M750', 'VBFHToWWToLNuQQ_M800', 'VBFHToWWToLNuQQ_M850', 'VBFHToWWToLNuQQ_M900', 'VBFHToWWToLNuQQ_M1000', 'VBFHToWWToLNuQQ_M1500', 'VBFHToWWToLNuQQ_M2000', 'VBFHToWWToLNuQQ_M2500', 'VBFHToWWToLNuQQ_M3000', 'VBFHToWWToLNuQQ_M4000', 'VBFHToWWToLNuQQ_M5000']
                },
+
+    'assignRun': {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : True  ,
+                  'import'     : 'LatinoAnalysis.NanoGardener.modules.RunAssigner' ,
+                  'declare'    : 'assignRun = lambda: RunAssigner("RPLME_CMSSW")',
+                  'module'     : 'assignRun()',
+            },
 
 ## ------- MODULES: Object Handling
 
