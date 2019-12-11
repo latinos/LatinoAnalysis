43a49bb085d66cf4bc846137c5af6c2326c9e0b1
scodella <luca.scodellaro@cern.ch>
2019-12-11 12:54:47 +0100

    This is becouse sometimes gridui machines at ifca change name...

--- NanoGardener/python/framework/Sites_cfg.py
+++ NanoGardener/python/framework/Sites_cfg.py
@@ -14,7 +14,9 @@ Sites = {
               'lsCmd'       : 'ls' ,
               'mkDir'       : True ,
               'xrootdPath'  : 'root://eoscms.cern.ch/' ,
-              'treeBaseDir' : '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/' ,
+              #'treeBaseDir' : '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/' ,
+              'treeBaseDir' : '/eos/cms/store/user/scodella/SUSY/Nano/' ,
+              #'treeBaseDir' : '/eos/cms/store/caf/user/scodella/BTV/Nano/' ,
               'batchQueues' : ['8nh','1nd','2nd','1nw']
            } ,
 
@@ -32,6 +34,14 @@ Sites = {
 
 
   'ifca' : {
+              'lsCmd'       : 'ls' ,
+              'mkDir'       : True ,
+              'xrootdPath'  : '' ,
+              'srmPrefix'   : 'srm://srm01.ifca.es' ,
+              'treeBaseDir' : '/gpfs/projects/tier3data/LatinosSkims/RunII/Nano/' ,
+             },
+
+  'cloud' : {
               'lsCmd'       : 'ls' ,
               'mkDir'       : True ,
               'xrootdPath'  : '' ,
