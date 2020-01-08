cfbfaa6ccf52b1196ff8c55eec3c21d2546a90db
Davide Valsecchi <davide.valsecchi@cern.ch>
2020-01-08 15:08:00 +0100

    [VBSjjlnu] Added steps for 2016

--- NanoGardener/python/framework/Steps_cfg.py
+++ NanoGardener/python/framework/Steps_cfg.py
@@ -11,6 +11,7 @@ TwoL2NuSamples=[ 'GluGluHToWWTo2L2Nu_M115', 'GluGluHToWWTo2L2Nu_M120', 'GluGluHT
 
 LNuJJ_VBS_Samples_bkg = ['WmTo2J_ZTo2L_QCD','WmToLNu_WmTo2J_QCD','WmToLNu_ZTo2J_QCD','WpTo2J_WmToLNu_QCD','WpTo2J_ZTo2L_QCD','WpToLNu_WpTo2J_QCD','WpToLNu_ZTo2J_QCD','ZTo2L_ZTo2J_QCD','WpToLNu_WmTo2J_QCD','WWW','WWZ','WZZ','ZZZ','WWG','WLNuJJ_EWK','EWKZ2Jets_ZToLL_M-50','DYJetsToLL_M-5to50-LO','DYJetsToLL_M-10to50-LO','DYJetsToLL_M-50-LO','DYJetsToLL_M-10to50-LO_ext1','DYJetsToLL_M-50-LO_ext1','DYJetsToLL_M-50','DYJetsToLL_M-4to50_HT-100to200','DYJetsToLL_M-4to50_HT-100to200_ext1','DYJetsToLL_M-4to50_HT-200to400','DYJetsToLL_M-4to50_HT-200to400_ext1','DYJetsToLL_M-4to50_HT-400to600','DYJetsToLL_M-4to50_HT-400to600_ext1','DYJetsToLL_M-4to50_HT-600toInf','DYJetsToLL_M-4to50_HT-600toInf_ext1','DYJetsToLL_M-50_HT-100to200','DYJetsToLL_M-50_HT-200to400','DYJetsToLL_M-50_HT-200to400_ext1','DYJetsToLL_M-50_HT-400to600_ext1','DYJetsToLL_M-50_HT-600to800','DYJetsToLL_M-50_HT-800to1200','DYJetsToLL_M-50_HT-1200to2500','DYJetsToLL_M-50_HT-2500toInf','ST_t-channel_top','ST_t-channel_antitop','ST_tW_antitop','ST_tW_top','ST_s-channel','TTTo2L2Nu','TTToSemiLeptonic','TTWjets','TTZjets','TTWjets_ext1','TTZjets_ext1','WJetsToLNu-LO','WJetsToLNu_HT100_200','WJetsToLNu_HT200_400','WJetsToLNu_HT400_600','WJetsToLNu_HT600_800','WJetsToLNu_HT800_1200','WJetsToLNu_HT1200_2500','WJetsToLNu_HT2500_inf','WJetsToLNu-LO_ext1','QCD_Pt-15to20_MuEnrichedPt5','QCD_Pt-20to30_MuEnrichedPt5','QCD_Pt-30to50_MuEnrichedPt5','QCD_Pt-50to80_MuEnrichedPt5','QCD_Pt-80to120_MuEnrichedPt5','QCD_Pt-120to170_MuEnrichedPt5','QCD_Pt-170to300_MuEnrichedPt5','QCD_Pt-20to30_EMEnriched','QCD_Pt-30to50_EMEnriched','QCD_Pt-50to80_EMEnriched']
 LNuJJ_VBS_Samples_signal = [ 'WmTo2J_ZTo2L','WmToLNu_WmTo2J','WmToLNu_ZTo2J','WpTo2J_WmToLNu','WpTo2J_ZTo2L','WpToLNu_WpTo2J','WpToLNu_ZTo2J','ZTo2L_ZTo2J','WpToLNu_WmTo2J']
+LNuJJ_VBS_Samples_data2016 = ['SingleElectron_Run2016B-Nano1June2019_ver2-v1','SingleElectron_Run2016C-Nano1June2019-v1','SingleElectron_Run2016D-Nano1June2019-v1','SingleElectron_Run2016E-Nano1June2019-v1', 'SingleElectron_Run2016F-Nano1June2019-v1','SingleElectron_Run2016G-Nano1June2019-v1', 'SingleElectron_Run2016H-Nano1June2019-v1','SingleMuon_Run2016B-Nano1June2019_ver2-v1','SingleMuon_Run2016C-Nano1June2019-v1','SingleMuon_Run2016D-Nano1June2019-v1','SingleMuon_Run2016E-Nano1June2019-v1', 'SingleMuon_Run2016F-Nano1June2019-v1','SingleMuon_Run2016G-Nano1June2019-v1', 'SingleMuon_Run2016H-Nano1June2019-v1']
 LNuJJ_VBS_Samples_data2017 = ['SingleElectron_Run2017B-Nano1June2019-v1','SingleElectron_Run2017C-Nano1June2019-v1','SingleElectron_Run2017D-Nano1June2019-v1','SingleElectron_Run2017E-Nano1June2019-v1','SingleElectron_Run2017F-Nano1June2019-v1','SingleMuon_Run2017B-Nano1June2019-v1','SingleMuon_Run2017C-Nano1June2019-v1','SingleMuon_Run2017D-Nano1June2019-v1','SingleMuon_Run2017E-Nano1June2019-v1','SingleMuon_Run2017F-Nano1June2019-v1']
 LNuJJ_VBS_Samples_data2018 = ['SingleMuon_Run2018A-Nano25Oct2019-v1','SingleMuon_Run2018B-Nano25Oct2019-v1','SingleMuon_Run2018C-Nano25Oct2019-v1','SingleMuon_Run2018D-Nano25Oct2019_ver2-v1','EGamma_Run2018A-Nano25Oct2019-v1','EGamma_Run2018B-Nano25Oct2019-v1','EGamma_Run2018C-Nano25Oct2019-v1','EGamma_Run2018D-Nano25Oct2019_ver2-v1']
 # -------------------------------------------- HERE WE GO ----------------------------------------------------
@@ -3239,6 +3240,34 @@ Steps = {
                         "',  
       'subTargets': ['CleanFatJet', 'VBSjjlnu_pairing', 'VBSjjlnu_kin'],
       'onlySample' : LNuJJ_VBS_Samples_bkg + LNuJJ_VBS_Samples_signal + LNuJJ_VBS_Samples_data2017
+  },
+
+  'VBSjjlnuSkim2016v4' : {
+      'isChain'    : True ,
+      'do4MC'      : True  ,
+      'do4Data'    : True  ,
+      'selection'  : '"nLepton>=1  && Lepton_pt[0]>30 \
+                          && (  Lepton_isTightElectron_mva_90p_Iso2016[0] > 0.5 \
+                             || Lepton_isTightMuon_cut_Tight80x[0] > 0.5 ) \
+                        && Alt$(Lepton_pt[1],0)<=10 && Alt$(Lepton_isLoose[1],1)> 0.5 \
+                         && (  Alt$(Lepton_isTightElectron_mva_90p_Iso2016[1], 0) < 0.5 \
+                             && Alt$(Lepton_isTightMuon_cut_Tight80x[1],0) < 0.5 )  \
+                        "',  
+      'subTargets': ['trigMC', 'CleanFatJet', 'CorrFatJetMass', 'VBSjjlnu_pairing', 'VBSjjlnu_kin'],
+      'onlySample' : LNuJJ_VBS_Samples_bkg + LNuJJ_VBS_Samples_signal
+  },
+
+  'VBSjjlnuSkim2016v4_data' : {
+      'isChain'    : True ,
+      'do4MC'      : True  ,
+      'do4Data'    : True  ,
+      'selection'  : '"nLepton>=1  && Lepton_pt[0]>30 \
+                        && Alt$(Lepton_pt[1],0)<=10 && Alt$(Lepton_isLoose[1],1)> 0.5 \
+                         && (  Alt$(Lepton_isTightElectron_mva_90p_Iso2016[1], 0) < 0.5 \
+                             && Alt$(Lepton_isTightMuon_cut_Tight80x[1],0) < 0.5 )  \
+                        "',  
+      'subTargets': ['fakeWstep1l','CleanFatJet', 'VBSjjlnu_pairing', 'VBSjjlnu_kin'],
+      'onlySample' : LNuJJ_VBS_Samples_data2016
   },
 
   'VBSjjlnuSkim2017v4' : {
