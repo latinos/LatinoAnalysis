9bf449e06ea10a2b7bbf36ada63b9b7965cc2896
Davide Valsecchi <davide.valsecchi@cern.ch>
2020-01-17 16:04:09 +0100

    fixed unfold_steps

--- NanoGardener/python/framework/unfold_steps.py
+++ NanoGardener/python/framework/unfold_steps.py
@@ -2,5 +2,6 @@ import json
 import Steps_cfg as steps
 
 output = { k:v for k, v in steps.Steps.items() if "VBSjjlnu" in k}
+print(output)
 
-json.dumps(output, open("Steps_cfg_unfold.py", "w"), indent=4)
+json.dump(output, open("Steps_cfg_unfold.py", "w"), indent=4)
