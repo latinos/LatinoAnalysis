bb4c111fa768c6e87114fcb7204de749c2299ebd
scodella <luca.scodellaro@cern.ch>
2020-02-24 13:10:07 +0100

    Susy nAODv6 signal

---
+++ NanoGardener/python/framework/samples/fall17FS_102X_nAODv6.py
@@ -0,0 +1,26 @@
+
+Samples = {}
+
+
+#### T2tt
+
+#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Fall17FS_102X_nAODv6/T2tt__mStop-150to250/']}
+#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Fall17FS_102X_nAODv6/T2tt__mStop-250to350/']}
+#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Fall17FS_102X_nAODv6/T2tt__mStop-350to400/']}
+#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Fall17FS_102X_nAODv6/T2tt__mStop-400to1200/']}
+#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Fall17FS_102X_nAODv6/T2tt__mStop-1200to2000/']}
+
+#### T2bW
+
+Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Fall17FS_102X_nAODv6/T2bW/']}
+
+#### TChipm
+
+Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Fall17FS_102X_nAODv6/TChipmSlepSnu/']}
+Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Fall17FS_102X_nAODv6/TChipmSlepSnu_mC1_825_1500/']}
+
+Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Fall17FS_102X_nAODv6/TChipmWW/']}
+
+
+
+
