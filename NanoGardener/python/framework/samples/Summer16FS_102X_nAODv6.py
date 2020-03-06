b864a12a7ce4f0340a484dfdf14d40a4419c2e3c
scodella <luca.scodellaro@cern.ch>
2020-03-06 15:39:43 +0100

    update susy signal nAODv6 samples

--- NanoGardener/python/framework/samples/Summer16FS_102X_nAODv6.py
+++ NanoGardener/python/framework/samples/Summer16FS_102X_nAODv6.py
@@ -4,23 +4,27 @@ Samples = {}
 
 #### T2tt
 
-#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-150to250/']}
-#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-250to350/']}
-#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-350to400/']}
-Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-400to1200/']}
-#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-1200to2000/']}
+Samples['T2tt__mStop-150to250']       = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-150to250/']}
+Samples['T2tt__mStop-250to350']       = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-250to350/']}
+Samples['T2tt__mStop-350to400']       = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-350to400/']}
+Samples['T2tt__mStop-400to1200']      = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-400to1200/']}
+Samples['T2tt__mStop-1200to2000']     = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2tt__mStop-1200to2000/']}
 
 #### T2bW
 
-#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2bW/']}
+Samples['T2bW']                       = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/T2bW/']}
 
 #### TChipm
 
-#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/TChipmSlepSnu/']}
-#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/TChipmSlepSnu_mC1_825_1500/']}
+Samples['TChipmSlepSnu']              = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/TChipmSlepSnu/']}
+Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/TChipmSlepSnu_mC1_825_1500/']}
 
-#Samples['TChipmSlepSnu_mC1_825_1500'] = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/TChipmWW/']}
+#### TChipmWW
 
+Samples['TChipmWW']                   = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/TChipmWW/']}
 
+#### TSlepSlep
 
+Samples['TSlepSlep']                  = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/TSlepSlep/']}
+Samples['TSlepSlep_mSlep-500to1300']  = {'srmPrefix': 'srm://srm01.ifca.es:8444/srm/managerv2?SFN=/cms', 'paths' : ['/store/group/phys_higgs/cmshww/amassiro/NanoProd/Summer16FS_102X_nAODv6/TSlepSlep_mSlep-500to1300/']}
 
