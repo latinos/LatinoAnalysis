d7c023511bbd9b7c1f7e24bbedcaca135bcde091
scodella <luca.scodellaro@cern.ch>
2020-03-17 19:52:38 +0100

    modify batchTools to use new batch system at ifca gridui

--- Tools/python/batchTools.py
+++ Tools/python/batchTools.py
@@ -377,7 +377,8 @@ class batchJobs :
          # We write the JDS file for documentation / resubmission, but initial submission will be done in one go below
          jobid=os.system('condor_submit '+jdsFileName+' > ' +jidFile)
        elif 'ifca' in hostName or 'cloud' in hostName:
-         jobid=os.system('qsub -P l.gaes -S /bin/bash -cwd -N Latino -o '+outFile+' -e '+errFile+' '+jobFile+' -j y > '+jidFile)
+           #jobid=os.system('qsub -P l.gaes -S /bin/bash -cwd -N Latino -o '+outFile+' -e '+errFile+' '+jobFile+' -j y > '+jidFile)
+           jobid=os.system('sbatch -o '+outFile+' -e '+errFile+' --qos=gridui_sort --partition=cloudcms '+jobFile+' > '+jidFile)
        elif "pi.infn.it" in socket.getfqdn():
          queue="cms"
          jobid=os.system('bsub -q '+queue+' -o '+outFile+' -e '+errFile+' '+jobFile+' > '+jidFile)
