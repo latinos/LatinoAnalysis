aa485ca6231d1b41425d036c7b6fe16a66fc99d5
Janssen Xavier <xavier.janssen@uantwerpen.be>
2020-05-01 14:07:48 +0200

    soem

--- NanoGardener/python/framework/Steps_cfg.py
+++ NanoGardener/python/framework/Steps_cfg.py
@@ -809,6 +809,68 @@ Steps = {
 
               }, 
 
+  'addTnPMuon' : {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : True ,
+                  'import'   : 'LatinoAnalysis.NanoGardener.modules.addTnpTree' ,
+                  'declare'  : 'TnPMu = lambda : addTnpTree(int("RPLME_YEAR"),"Muon")', 
+                  'module'   : 'TnPMu()', 
+                  'onlySample' : ['DYJetsToLL_M-50-LO_ext1','DYJetsToLL_M-50_ext1','DYJetsToLL_M-50_ext2',
+                                  # Run2016 v6
+                                  'SingleMuon_Run2016B-Nano25Oct2019_ver2-v1',
+                                  'SingleMuon_Run2016C-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2016D-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2016E-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2016F-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2016G-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2016H-Nano25Oct2019-v1',
+                                  # Run2017 v6
+                                  'SingleMuon_Run2017B-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2017C-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2017D-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2017E-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2017F-Nano25Oct2019-v1',
+                                  # Run2018 v6
+                                  'SingleMuon_Run2018A-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2018B-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2018C-Nano25Oct2019-v1',
+                                  'SingleMuon_Run2018D-Nano25Oct2019_ver2-v1'
+                                 ] ,
+
+                 },
+
+  'addTnPEle' : {
+                  'isChain'    : False ,
+                  'do4MC'      : True  ,
+                  'do4Data'    : True ,
+                  'import'   : 'LatinoAnalysis.NanoGardener.modules.addTnpTree' ,
+                  'declare'  : 'TnPEle = lambda : addTnpTree(int("RPLME_YEAR"),"Electron")',
+                  'module'   : 'TnPEle()', 
+                  'onlySample' : ['DYJetsToLL_M-50-LO_ext1','DYJetsToLL_M-50_ext1','DYJetsToLL_M-50_ext2',
+                                  # Run2016 v6
+                                  'SingleElectron_Run2016B-Nano25Oct2019_ver2-v1',
+                                  'SingleElectron_Run2016C-Nano25Oct2019-v1',
+                                  'SingleElectron_Run2016D-Nano25Oct2019-v1',
+                                  'SingleElectron_Run2016E-Nano25Oct2019-v1',
+                                  'SingleElectron_Run2016F-Nano25Oct2019-v1',
+                                  'SingleElectron_Run2016G-Nano25Oct2019-v1',
+                                  'SingleElectron_Run2016H-Nano25Oct2019-v1',
+                                  # Run2017 v6
+                                  'SingleElectron_Run2017B-Nano25Oct2019-v1',
+                                  'SingleElectron_Run2017C-Nano25Oct2019-v1',
+                                  'SingleElectron_Run2017D-Nano25Oct2019-v1',
+                                  'SingleElectron_Run2017E-Nano25Oct2019-v1',
+                                  'SingleElectron_Run2017F-Nano25Oct2019-v1',
+                                  # Run2018 v6
+                                  'EGamma_Run2018A-Nano25Oct2019-v1',
+                                  'EGamma_Run2018B-Nano25Oct2019-v1',
+                                  'EGamma_Run2018C-Nano25Oct2019-v1',
+                                  'EGamma_Run2018D-Nano25Oct2019_ver2-v1',
+                                 ] ,
+
+                 },
+
 ## ------- WgStar MC:
 
   'MCWgStar2017' : { 
