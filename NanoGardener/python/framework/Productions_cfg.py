9e3016d8dd310e58e9ad33f69bcb22ca632d0c0b
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2021-05-04 15:27:24 +0200

    Add UL productions

--- NanoGardener/python/framework/Productions_cfg.py
+++ NanoGardener/python/framework/Productions_cfg.py
@@ -570,6 +570,34 @@ Productions = {
                        'year'    : '2018' ,
                    },
 
+#################################### nAODv8 UL DATA  ##############################################
+ # -------- 2016 DATA UL2016 nAODv8: Full2016v8
+ 'Run2016_UL2016_nAODv8_Full2016v8': {
+                       'isData'  : True ,
+                       'jsonFile'   : '"%s/src/LatinoAnalysis/NanoGardener/python/data/certification/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"  % os.environ["CMSSW_BASE"]',
+                       'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2016_UL2016_nAODv8.py' ,
+                       'cmssw'   : 'Full2016v8',
+                       'year'    : '2016' ,
+                   },
+
+ # -------- 2017 DATA UL2017 nAODv8: Full2017v8
+ 'Run2017_UL2017_nAODv8_Full2017v8': {
+                       'isData'  : True ,
+                       'jsonFile'   : '"%s/src/LatinoAnalysis/NanoGardener/python/data/certification/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt"  % os.environ["CMSSW_BASE"]',
+                       'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2017_UL2017_nAODv8.py' ,
+                       'cmssw'   : 'Full2017v8',
+                       'year'    : '2017' ,
+                   },
+
+ # -------- 2018 DATA UL2018 nAODv8: Full2018v8
+ 'Run2018_UL2018_nAODv8_Full2018v8': {
+                       'isData'  : True ,
+                       'jsonFile'   : '"%s/src/LatinoAnalysis/NanoGardener/python/data/certification/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt"  % os.environ["CMSSW_BASE"]',
+                       'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2018_UL2018_nAODv8.py' ,
+                       'cmssw'   : 'Full2018v8',
+                       'year'    : '2018' ,
+                   },
+
 #################################### nAODv6 MC ##############################################
 
  # -------- 2016 MC 102X nAODv7: Full2016v7
