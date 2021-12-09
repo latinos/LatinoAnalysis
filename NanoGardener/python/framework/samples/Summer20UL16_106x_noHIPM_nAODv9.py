0581380858e0dbbc643f680dfbd7949f5ca3a674
Mattia Lizzo <mattia.lizzo@cern.ch>
2021-12-09 14:23:00 +0100

    DYJetsToLL_M-50 NLO sample added

--- NanoGardener/python/framework/samples/Summer20UL16_106x_noHIPM_nAODv9.py
+++ NanoGardener/python/framework/samples/Summer20UL16_106x_noHIPM_nAODv9.py
@@ -101,6 +101,10 @@ Samples['WJetsToLNu_2J'] = {'nanoAOD' :'/WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXF
 Samples['DYJetsToTT_MuEle_M-50'] = {'nanoAOD' :'/DYJetsToTauTau_TauToMuEle_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v1/NANOAODSIM'} ##!!!!
 ##---------------------------------------------------------------##
 #--->Updated!###-----------------------------------------------##
+Samples['DYJetsToLL_M-50'] = {'nanoAOD' :'/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v1/NANOAODSIM'}
+##---------------------------------------------------------------##
+
+#--->Updated!###-----------------------------------------------##
 Samples['DYJetsToLL_M-50_HT-70to100'] = {'nanoAOD' :'/DYJetsToLL_M-50_HT-70to100_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v2/NANOAODSIM'} ##!!!!
 ##---------------------------------------------------------------##
 #--->Updated!###-----------------------------------------------##
