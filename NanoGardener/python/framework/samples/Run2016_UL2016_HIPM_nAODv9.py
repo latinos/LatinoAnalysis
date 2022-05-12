2a8ea9704522a671dd7bcfc5750c7f2c58d75b9f
xjanssen <xjanssen@cern.ch>
2022-05-12 14:28:11 +0200

    Up

--- NanoGardener/python/framework/samples/Run2016_UL2016_HIPM_nAODv9.py
+++ NanoGardener/python/framework/samples/Run2016_UL2016_HIPM_nAODv9.py
@@ -1,11 +1,11 @@
 Samples = {}
 
-#Samples["DoubleEG_Run2016B-ver1_HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016B-ver1_HIPM_UL2016_MiniAODv2_NanoAODv9-v1//NANOAOD'}
+Samples["DoubleEG_Run2016B-ver1_HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016B-ver1_HIPM_UL2016_MiniAODv2_NanoAODv9-v1//NANOAOD'}
 Samples["DoubleEG_Run2016B-ver2_HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016B-ver2_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
-#Samples["DoubleEG_Run2016C-HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016C-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
-#Samples["DoubleEG_Run2016D-HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016D-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
-#Samples["DoubleEG_Run2016E-HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016E-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
-#Samples["DoubleEG_Run2016F-HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016F-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+Samples["DoubleEG_Run2016C-HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016C-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+Samples["DoubleEG_Run2016D-HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016D-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+Samples["DoubleEG_Run2016E-HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016E-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+Samples["DoubleEG_Run2016F-HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleEG/Run2016F-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 
 #Samples["DoubleMuon_Run2016B-ver1_HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleMuon/Run2016B-ver1_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 #Samples["DoubleMuon_Run2016B-ver2_HIPM_UL2016-v1"] = {'nanoAOD': '/DoubleMuon/Run2016B-ver2_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
