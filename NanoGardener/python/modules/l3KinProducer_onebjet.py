7d6a1c5f8cd1d428a16fefa622ec096a18969211
sahithi <srudra4@uic.edu>
2023-08-24 21:51:14 +0200

    l3Kin one bjet AZH update

--- NanoGardener/python/modules/l3KinProducer_onebjet.py
+++ NanoGardener/python/modules/l3KinProducer_onebjet.py
@@ -356,18 +356,41 @@ class l3KinProducer(Module):
             return -9999
 #------------------------------------------------------------------------------------------------------------------------------------------------------
 
+    def AZH_mA_minus_mH_onebjet(self):
+        if self.AZH_isOk_onebjet:
+            return (self.ZH3l_XLepton[0] + self.AZH_Neutrino_best_onebjet + self.bJetLeptonic_best_onebjet + self.bJetHadronic_best_onebjet + self.WJet1_best_onebjet + self.WJet2_best_onebjet + self.Zlepton1 + self.Zlepton2).M()-(self.ZH3l_XLepton[0] + self.AZH_Neutrino_best_onebjet + self.bJetLeptonic_best_onebjet + self.bJetHadronic_best_onebjet + self.WJet1_best_onebjet + self.WJet2_best_onebjet).M()
+        else:
+            return -9999
 
+    def AZH_Amass_onebjet(self):
+        if self.AZH_isOk_onebjet:
+            return (self.ZH3l_XLepton[0] + self.AZH_Neutrino_best_onebjet + self.bJetLeptonic_best_onebjet + self.bJetHadronic_best_onebjet + self.WJet1_best_onebjet + self.WJet2_best_onebjet + self.Zlepton1 + self.Zlepton2).M()
+        else:
+            return -9999
 
+    def AZH_Hmass_onebjet(self):
+        if self.AZH_isOk_onebjet:
+            return (self.ZH3l_XLepton[0] + self.AZH_Neutrino_best_onebjet + self.bJetLeptonic_best_onebjet + self.bJetHadronic_best_onebjet + self.WJet1_best_onebjet + self.WJet2_best_onebjet).M()
+        else:
+            return -9999
 
+    def AZH_ChiSquare_onebjet(self):
+        if self.AZH_isOk_onebjet:
+            return self.ChisqMin_onebjet
+        else:
+            return -9999
 
+    def AZH_Tophadronic_onebjet(self):
+        if self.AZH_isOk_onebjet:
+            return (self.bJetHadronic_best_onebjet + self.WJet1_best_onebjet + self.WJet2_best_onebjet).M()
+        else:
+            return -9999
 
-
-
-
-
-
-
-
+    def AZH_Topleptonic(self):
+        if self.AZH_isOk_onebjet:
+            return (self.ZH3l_XLepton[0] + self.AZH_Neutrino_best_onebjet + self.bJetLeptonic_best_onebjet).M()
+        else:
+            return -9999
 
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
