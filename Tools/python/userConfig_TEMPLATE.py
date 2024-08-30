c9f754552ab156998b3ddd99e47029fa9ee0ab21
acmbulla <a.bulla@campus.unimib.it>
2024-08-30 14:00:28 +0200

    job py files in eos, condor log&err to /dev/null option

--- Tools/python/userConfig_TEMPLATE.py
+++ Tools/python/userConfig_TEMPLATE.py
@@ -2,4 +2,9 @@
 baseDir  = '/afs/cern.ch/user/x/xjanssen/cms/HWW2015/'
 jobDir   = baseDir+'jobs/'
 workDir  = baseDir+'workspace/'
+
+baseDirPy  = '/eos/user/a/abulla/private/'
+jobDirPy   = baseDirPy+'jobs/'
+workDirPy  = baseDirPy+'workspace/'
+
 jobDirSplit = True
